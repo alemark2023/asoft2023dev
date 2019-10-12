088a4f3701a71146ff7b702916255b76646bfd94
Cristian Gonzalez <cristian.ballon@gmail.com>
2019-10-11 19:07:26 -0500

    insert en seeder tenant items

--- database/seeds/tenants/TenancyDatabaseSeeder.php
+++ database/seeds/tenants/TenancyDatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class TenancyDatabaseSeeder extends Seeder
 {
@@ -11,5 +13,28 @@ class TenancyDatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        DB::table('items')->insert([
+
+            ['name' => 'Laptop Razer', 'second_name' => 'Laptop Razer', 'description' => 'Laptop Razer','item_type_id' => '01', 
+            'unit_type_id' => 'NIU', 'currency_type_id' => 'PEN', 'sale_unit_price' => '5500.00', 'has_igv' => 1, 'purchase_unit_price' => '3000.00', 'has_isc' => 0,
+            'amount_plastic_bag_taxes' => '0.10', 'percentage_isc' => '0.00', 'suggested_price' => '0.00', 'sale_affectation_igv_type_id' => '10' , 
+            'purchase_affectation_igv_type_id' => '10', 'calculate_quantity' => '0', 'image' => 'demo1.jpg', 'image_medium' => 'demo1_medium.jpg', 'image_small' => 'demo1_small.jpg',  'stock' => '1',
+            'stock_min' => '1',  'percentage_of_profit' => '20.0',  'has_perception' => '0',  'status' => 1, 'apply_store' => 1 ],
+
+            ['name' => 'MacBook Pro', 'second_name' => 'LMacBook Pro', 'description' => 'MacBook Pro','item_type_id' => '01', 
+            'unit_type_id' => 'NIU', 'currency_type_id' => 'PEN', 'sale_unit_price' => '5500.00', 'has_igv' => 1, 'purchase_unit_price' => '3000.00', 'has_isc' => 0,
+            'amount_plastic_bag_taxes' => '0.10', 'percentage_isc' => '0.00', 'suggested_price' => '0.00', 'sale_affectation_igv_type_id' => '10' , 
+            'purchase_affectation_igv_type_id' => '10', 'calculate_quantity' => '0', 'image' => 'demo2.jpg', 'image_medium' => 'demo2_medium.jpg', 'image_small' => 'demo2_small.jpg',  'stock' => '1',
+            'stock_min' => '1',  'percentage_of_profit' => '20.0',  'has_perception' => '0',  'status' => 1, 'apply_store' => 1 ],
+
+            ['name' => 'Laptop Asus', 'second_name' => 'Laptop Asus', 'description' => 'Laptop Asus','item_type_id' => '01', 
+            'unit_type_id' => 'NIU', 'currency_type_id' => 'PEN', 'sale_unit_price' => '5500.00', 'has_igv' => 1, 'purchase_unit_price' => '3000.00', 'has_isc' => 0,
+            'amount_plastic_bag_taxes' => '0.10', 'percentage_isc' => '0.00', 'suggested_price' => '0.00', 'sale_affectation_igv_type_id' => '10' , 
+            'purchase_affectation_igv_type_id' => '10', 'calculate_quantity' => '0', 'image' => 'demo3.jpg', 'image_medium' => 'demo3_medium.jpg', 'image_small' => 'demo3_small.jpg',  'stock' => '1',
+            'stock_min' => '1',  'percentage_of_profit' => '20.0',  'has_perception' => '0',  'status' => 1, 'apply_store' => 1 ],
+          
+        ]);
+
     }
 }
