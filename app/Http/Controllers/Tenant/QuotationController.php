a91a934dc747780cf74745765d7e0bf854503cf6
Cristian Gonzalez <cristian.ballon@gmail.com>
2019-10-18 18:21:16 -0500

    valid dispatches

--- app/Http/Controllers/Tenant/QuotationController.php
+++ app/Http/Controllers/Tenant/QuotationController.php
@@ -166,7 +166,6 @@ class QuotationController extends Controller
 
     public function store(QuotationRequest $request) {
 
-        return 1;
         DB::connection('tenant')->transaction(function () use ($request) {
             $data = $this->mergeData($request);
             
