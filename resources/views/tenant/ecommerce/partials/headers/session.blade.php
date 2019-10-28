d2706f429ce9fb2c38e03fcd57ecb26935b63add
Cristian Gonzalez <cristian.ballon@gmail.com>
2019-10-28 08:36:04 -0500

    estilos header eccomerce

--- resources/views/tenant/ecommerce/partials/headers/session.blade.php
+++ resources/views/tenant/ecommerce/partials/headers/session.blade.php
@@ -1,6 +1,7 @@
 <style>
     .btn-logout {
         font-size: 25px;
+        margin-left: 4%;
     }
 
 </style>
@@ -9,61 +10,28 @@
 
     @guest
     <div class="header-contact">
-        <a class="login-link" href="{{route('tenant_ecommerce_login')}}"><strong style="font-size: 16px;">LOG IN</strong></a>
+        <a class="login-link" href="{{route('tenant_ecommerce_login')}}"><strong style="font-size: 16px;">LOG
+                IN</strong></a>
     </div>
     @else
     <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
         data-display="static">
-        <i class="icon-user fa-2x text-white"></i>
-        <div class="dropdown-menu">
-            <div class="dropdownmenu-wrapper">
-
-                <div class="dropdown-cart-total">
-                    <span>{{ Auth::user()->email }} </span>
-                    <a href="#" role="menuitem" class="btn-logout" data-toggle="tooltip" data-placement="bottom" title="Cerrar Session"
-                        onclick="event.preventDefault(); logout();">
-                        <i class="fas fa-power-off"></i>
-                    </a>
-
-                </div>
-               
-            </div>
-        </div>
-
-        @endguest
-
-
-
-
-
-        {{--<a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
-    	<i class="icon-user fa-2x text-white"></i>
-    </a>
+        <i class="icon-user fa-2x text-white"></i> </a>
     <div class="dropdown-menu">
         <div class="dropdownmenu-wrapper">
-           
+
             <div class="dropdown-cart-total">
-                @guest
-                	<span>Invitado</span>
-                @else
-                	<span>{{ Auth::user()->email }}</span>
-        @endguest
-</div><!-- End .dropdown-cart-total -->
+                <span>{{ Auth::user()->email }} </span>
+                <a href="#" role="menuitem" class="btn-logout" data-toggle="tooltip" data-placement="bottom"
+                    title="Cerrar Session" onclick="event.preventDefault(); logout();">
+                    <i class="fas fa-power-off"></i>
+                </a>
 
-<div class="dropdown-cart-action text-right">
+            </div>
 
-    @guest
-    <a href="{{route('tenant_ecommerce_login')}}" class="btn">Ingresar</a>
-    @else
-    <a role="menuitem" href="{{ route('logout') }}" class="btn"
-        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-        Salir
-    </a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
+        </div>
+    </div>
+
     @endguest
-</div>
-</div>
-</div> --}}
+
 </div>
