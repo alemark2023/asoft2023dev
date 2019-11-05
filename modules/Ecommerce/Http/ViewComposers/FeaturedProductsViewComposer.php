5964cba9973898178f5d4c9b1d56988ef4ff111c
cristian gonzalez <cristian.ballon@gmail.com>
2019-11-05 16:09:45 -0500

    modulo Ecommerce

---
+++ modules/Ecommerce/Http/ViewComposers/FeaturedProductsViewComposer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Ecommerce\Http\ViewComposers;
+
+
+use App\Models\Tenant\Item;
+//use App\Http\Resources\Tenant\ItemEcommerceCollection;
+
+
+class FeaturedProductsViewComposer
+{
+    public function compose($view)
+    {
+        $view->items = Item::where('apply_store', 1)->get()->transform(function($row, $key){
+            return (object)[
+                'id' => $row->id,
+                'description' => $row->description,
+                'name' => $row->name,
+                'second_name' => $row->second_name,
+                'sale_unit_price' => $row->sale_unit_price,
+                'image' =>  $row->image,
+                'image_medium' => $row->image_medium,
+                'image_small' => $row->image_small,
+                'tags' => $row->tags->pluck('tag_id')->toArray()
+            ];
+        });
+    }
+}
