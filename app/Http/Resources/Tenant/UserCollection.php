a916ab9d18c7204406ad41d726477c3ab2827eef
Cristian Gonzalez <cristian.ballon@gmail.com>
2019-11-12 11:03:38 -0500

    tipo user en lista users

--- app/Http/Resources/Tenant/UserCollection.php
+++ app/Http/Resources/Tenant/UserCollection.php
@@ -15,13 +15,30 @@ class UserCollection extends ResourceCollection
     public function toArray($request)
     {
         return $this->collection->transform(function($row, $key) {
+
+            $type = '';
+            switch ($row->type) {
+                case 'admin':
+                    $type =  'Administrador' ;
+                    break;
+                case 'seller':
+                    $type =  'Vendedor' ;
+                        break;
+                case 'client':
+                    $type =  'Cliente' ;
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+
             return [
                 'id' => $row->id,
                 'email' => $row->email,
                 'name' => $row->name,
                 'api_token' => $row->api_token,
                 'establishment_description' => optional($row->establishment)->description,
-                'type' => ($row->type == 'admin') ? 'Administrador' : 'Vendedor',
+                'type' => $type,
                 'locked' => (bool) $row->locked,
 
             ];
