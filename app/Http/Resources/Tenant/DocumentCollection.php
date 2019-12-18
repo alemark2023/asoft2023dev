33d9d4f947386baefd5d927774f14e9442b94b38
Cristian Gonzalez <cristian.ballon@gmail.com>
2019-12-18 16:40:09 -0500

    add  customer_telephone api list document

--- app/Http/Resources/Tenant/DocumentCollection.php
+++ app/Http/Resources/Tenant/DocumentCollection.php
@@ -119,7 +119,7 @@ class DocumentCollection extends ResourceCollection
                 'updated_at' => $row->updated_at->format('Y-m-d H:i:s'),
                 'user_name' => ($row->user) ? $row->user->name : '',
                 'user_email' => ($row->user) ? $row->user->email : '',
-                'external_id' => $row->external_id
+                'external_id' => $row->external_id,
 
                 'notes' => (in_array($row->document_type_id, ['01', '03'])) ? $row->affected_documents->transform(function($row) {
                     return [
