5e7f4e0a150f24092930ef455975e6064c02a78a
b.mendoza <bemendozaa@gmail.com>
2020-01-14 11:22:44 -0500

    fixed

---
+++ app/Providers/DocumentPaymentProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DocumentPaymentProvider extends ServiceProvider
+{
+     
+    public function register()
+    {
+    }
+    
+    public function boot()
+    {
+        
+    }
+
+
+}
