c72ede57df5422f5f6527dd96f1114fdcd7f76dc
Raul <sh.raul.jgb@gmail.com>
2020-01-29 12:54:24 -0500

    font_swz update logo max box

--- app/CoreFacturalo/Templates/pdf/font_swz/invoice_a4.blade.php
+++ app/CoreFacturalo/Templates/pdf/font_swz/invoice_a4.blade.php
@@ -27,7 +27,7 @@
         <td width="65%">
             @if($company->logo)
                 <div class="company_logo_box">
-                    <img src="data:{{mime_content_type(public_path("storage/uploads/logos/{$company->logo}"))}};base64, {{base64_encode(file_get_contents(public_path("storage/uploads/logos/{$company->logo}")))}}" alt="{{$company->name}}" class="company_logo" style="max-height: 80px;">
+                    <img src="data:{{mime_content_type(public_path("storage/uploads/logos/{$company->logo}"))}};base64, {{base64_encode(file_get_contents(public_path("storage/uploads/logos/{$company->logo}")))}}" alt="{{$company->name}}" class="company_logo">
                 </div>
             @endif
         </td>
