d6cf8ee8027f11781c25db0c37fc8aa25758d223
cr1sti4n <cristian.ballon@gmail.com>
2020-02-04 23:28:05 -0500

    menu admin

--- resources/views/system/layouts/partials/sidebar.blade.php
+++ resources/views/system/layouts/partials/sidebar.blade.php
@@ -50,7 +50,7 @@
                 <ul class="nav nav-main">
                     <li class="{{ ($path[0] === 'configurations')?'nav-active':'' }}">
                         <a class="nav-link" href="{{route('system.configuration.index')}}">
-                            <i class="fas fa-cogs"></i><span>Configuración</span>
+                            <i class="fas fa-cogs"></i><span>Cerfticado PSE</span>
                         </a>
                     </li>
                 </ul>
