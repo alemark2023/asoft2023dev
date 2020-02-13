317c069915901ec9f729fa208a563efc5862bcc8
cr1sti4n <cristian.ballon@gmail.com>
2020-02-12 21:05:08 -0500

    ecommer data contact

--- modules/Ecommerce/Http/Controllers/EcommerceController.php
+++ modules/Ecommerce/Http/Controllers/EcommerceController.php
@@ -210,7 +210,7 @@ class EcommerceController extends Controller
             $document->client = $user->name;
             $document->product = $request->producto;
             $document->total = $request->precio_culqi;
-           // Mail::to($customer_email)->send(new CulqiEmail($document));
+            Mail::to($customer_email)->send(new CulqiEmail($document));
             return [
                 'success' => true,
                 'order' => $order
