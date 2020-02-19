c7d13401487c7aff9823672c4085d98e5fcacf70
Raul <sh.raul.jgb@gmail.com>
2020-02-18 21:56:35 -0500

    t-279 + plus [configuraciones/avanzado]

--- resources/js/views/tenant/configurations/form.vue
+++ resources/js/views/tenant/configurations/form.vue
@@ -3,24 +3,31 @@
         <div class="card-header bg-info">
             <h3 class="my-0">Configuraciones</h3>
         </div>
-        <div class="card-body">
+        <div class="card-body pt-0 pb-5">
             <form autocomplete="off">
                 <div class="form-body">
                     <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="separator-title">Servicios</h3>
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-md-6">
-                            <label class="control-label">Reenvio de Facturas automático</label>
+                            <label class="control-label">Envío de comprobantes automático</label>
                             <div class="form-group" :class="{'has-danger': errors.send_auto}">
                                 <el-switch v-model="form.send_auto" active-text="Si" inactive-text="No" @change="submit"></el-switch>
                                 <small class="form-control-feedback" v-if="errors.send_auto" v-text="errors.send_auto[0]"></small>
                             </div>
                         </div>
                         <div class="col-md-6" v-if="typeUser != 'integrator'">
-                            <label class="control-label">Crontab</label>
+                            <label class="control-label">Crontab <small>Tareas Programadas</small></label>
                             <div class="form-group" :class="{'has-danger': errors.cron}">
                                 <el-switch v-model="form.cron" active-text="Si" inactive-text="No" @change="submit"></el-switch>
                                 <small class="form-control-feedback" v-if="errors.cron" v-text="errors.cron[0]"></small>
                             </div>
                         </div>
+                    </div>
+                    <div class="row">
                         <div class="col-md-6 mt-4" v-if="typeUser != 'integrator'">
                             <label class="control-label">Envío de comprobantes a servidor alterno de SUNAT</label>
                             <div class="form-group" :class="{'has-danger': errors.sunat_alternate_server}">
@@ -28,16 +35,14 @@
                                 <small class="form-control-feedback" v-if="errors.sunat_alternate_server" v-text="errors.sunat_alternate_server[0]"></small>
                             </div>
                         </div>
-                 
-                        <div class="col-md-6 mt-4" v-if="typeUser != 'integrator'">
-                            <label class="control-label">Menú lateral contraído</label>
-                            <div class="form-group" :class="{'has-danger': errors.compact_sidebar}">
-                                <el-switch v-model="form.compact_sidebar" active-text="Si" inactive-text="No" @change="compactSidebar"></el-switch>
-                                <small class="form-control-feedback" v-if="errors.compact_sidebar" v-text="errors.compact_sidebar[0]"></small>
-                            </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="separator-title">Contable</h3>
                         </div>
-                        
-                         <div class="col-md-4 mt-4" v-if="typeUser != 'integrator'">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4" v-if="typeUser != 'integrator'">
                             <label class="control-label">Cantidad decimales POS</label>
                             <div class="form-group" :class="{'has-danger': errors.decimal_quantity}">
                                 <el-input-number v-model="form.decimal_quantity" @change="submit" :min="2" :max="10"></el-input-number>
@@ -45,24 +50,39 @@
                             </div>
                         </div>
                         <div class="col-md-2"></div>
-
-                         <div class="col-md-4 mt-4" v-if="typeUser != 'integrator'">
+                        <div class="col-md-4" v-if="typeUser != 'integrator'">
                             <label class="control-label">Impuesto bolsa plástica</label>
                             <div class="form-group" :class="{'has-danger': errors.amount_plastic_bag_taxes}">
                                 <el-input-number v-model="form.amount_plastic_bag_taxes" @change="changeAmountPlasticBagTaxes" :precision="2" :step="0.1" :max="0.5" :min="0.1"></el-input-number>
                                 <small class="form-control-feedback" v-if="errors.amount_plastic_bag_taxes" v-text="errors.amount_plastic_bag_taxes[0]"></small>
                             </div>
                         </div>
-
-                         <div class="col-md-6 mt-6">
-                            <label>Escoge su formato</label>
-                             <el-select v-model="formato.formats" @change="changeFormat(formato.formats)">
-                                    <el-option disabled value="">Selecciona Formato</el-option>
-                                    <el-option v-for="(option, index) in formatos" :key="index" v-bind:value="option.formats"> {{option.formats}}</el-option>
-                                </el-select>
-                              <el-button type="success" @click="addSeeder" icon="el-icon-refresh" circle></el-button> 
-                                <small class="form-control-feedback" v-model="form.formats"> Formato actual: {{form.formats}}</small>
-                         </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="separator-title">Visual</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label>Plantillas PDF</label>
+                            <el-select v-model="formato.formats" @change="changeFormat(formato.formats)">
+                                <el-option disabled value="">Seleccione Plantilla</el-option>
+                                <el-option v-for="(option, index) in formatos" :key="index" v-bind:value="option.formats"> {{option.formats}}</el-option>
+                            </el-select>
+                            <small class="form-control-feedback" v-model="form.formats"> Plantilla actual: {{form.formats}}</small>
+                        </div>
+                        <div class="col-md-6">
+                            <label>Actualizar lista de plantillas</label><br>
+                            <el-button type="success" @click="addSeeder" icon="el-icon-refresh"></el-button>
+                        </div>
+                        <div class="col-md-6 mt-4" v-if="typeUser != 'integrator'">
+                            <label class="control-label">Menú lateral contraído</label>
+                            <div class="form-group" :class="{'has-danger': errors.compact_sidebar}">
+                                <el-switch v-model="form.compact_sidebar" active-text="Si" inactive-text="No" @change="compactSidebar"></el-switch>
+                                <small class="form-control-feedback" v-if="errors.compact_sidebar" v-text="errors.compact_sidebar[0]"></small>
+                            </div>
+                        </div>
                         <!-- <div class="col-md-6 mt-4" v-if="typeUser != 'integrator'">
                             <label class="control-label">Cuenta contable venta subtotal</label>
                             <div class="form-group" :class="{'has-danger': errors.subtotal_account}">
@@ -92,7 +112,7 @@
                     formats: ''
                 },
                 placeholder:'',
-                
+
             }
         },
         async created() {
@@ -102,7 +122,7 @@
                 if (response.data !== ''){
                 this.form = response.data.data;
                 this.placeholder = response.data.data.formats;
-                } 
+                }
                 console.log(this.placeholder)
             });
              await this.$http.get(`/${this.resource}/getFormats`) .then(response => {
@@ -112,7 +132,7 @@
         },
         methods: {
            addSeeder(){
-            var ruta = location.host 
+            var ruta = location.host
             location.href="/configurations/addSeeder"
             },
             changeFormat(value){
@@ -164,7 +184,7 @@
             },
             compactSidebar() {
                 this.loading_submit = true;
-                
+
                 this.$http.post(`/${this.resource}`, this.form).then(response => {
                     if (response.data.success) {
                         this.$message.success(response.data.message);
