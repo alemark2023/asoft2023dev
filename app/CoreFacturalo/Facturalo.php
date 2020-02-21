b2c31b3cba7497acbe099a6d86efe815c4a56315
Aisak <santaellaisaac2@gmail.com>
2020-02-20 20:20:42 -0400

    ticket 259

--- app/CoreFacturalo/Facturalo.php
+++ app/CoreFacturalo/Facturalo.php
@@ -273,7 +273,7 @@ class Facturalo
         
         $base_pdf_template = $configuration;//config(['tenant.pdf_template'=> $configuration]);
         // dd($base_pdf_template);
-
+   
 
         $html = $template->pdf($base_pdf_template, $this->type, $this->company, $this->document, $format_pdf);
 
@@ -323,7 +323,7 @@ class Facturalo
             }
             $legends = $this->document->legends != '' ? '10' : '0';
 
-            $pdf = new Mpdf([
+           dd( $pdf = new Mpdf([
                 'mode' => 'utf-8',
                 'format' => [
                     $width,
@@ -349,12 +349,13 @@ class Facturalo
                     $was_deducted_prepayment +
                     $customer_department_id+
                     $detraction+
-                    $total_plastic_bag_taxes],
+                    $total_plastic_bag_taxes
+                ],
                 'margin_top' => 0,
                 'margin_right' => 1,
                 'margin_bottom' => 0,
                 'margin_left' => 1
-            ]);
+            ]));
         }else if($format_pdf === 'a5'){
 
             $company_name      = (strlen($this->company->name) / 20) * 10;
