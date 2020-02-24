8551c81d60e04018e0be1c02a91a95d477aac4d2
b.mendoza <bemendozaa@gmail.com>
2020-02-24 17:58:23 -0500

    ajustes t 300

--- resources/js/views/tenant/documents/partials/options.vue
+++ resources/js/views/tenant/documents/partials/options.vue
@@ -1,6 +1,18 @@
 <template>
     <el-dialog :title="titleDialog" :visible="showDialog" @open="create" width="30%" :close-on-click-modal="false" :close-on-press-escape="false" :show-close="false" append-to-body>
+            
+        <div class="row mb-4" v-if="form.response_message">
+            <div class="col-md-12">
+                <el-alert
+                    :title="form.response_message"
+                    :type="form.response_type"
+                    show-icon>
+                </el-alert>
+            </div>
+        </div>
+
         <div class="row">
+
             <div class="col-lg-12 col-md-12 col-sm-12 text-center font-weight-bold" v-if="!locked_emission.success">
                 <el-alert    :title="locked_emission.message"    type="warning"    show-icon>  </el-alert>
             </div>
@@ -16,7 +28,7 @@
                 <button type="button" class="btn btn-lg btn-info waves-effect waves-light" @click="clickPrint('ticket')">
                     <i class="fa fa-receipt"></i>
                 </button>
-                 <p>Imprimir Ticket</p>
+                 <p>Imprimir Ticket 80MM</p>
             </div>
 
              <div class="col-lg-3 col-md-3 col-sm-12 text-center font-weight-bold mt-3">
@@ -50,15 +62,6 @@
                 </button>
             </div>
         </div> -->
-        <div class="row mt-4" v-if="form.response_message">
-            <div class="col-md-12">
-                <el-alert
-                    :title="form.response_message"
-                    :type="form.response_type"
-                    show-icon>
-                </el-alert>
-            </div>
-        </div>
         <div class="row mt-3">
             <div class="col-md-12">
                 <el-input v-model="form.customer_email">
