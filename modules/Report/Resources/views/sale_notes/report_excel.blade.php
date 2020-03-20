7ba9c7a1741958ae078316693be41545ff151deb
b.mendoza <bemendozaa@gmail.com>
2020-03-20 17:06:40 -0500

    t 491 - totales reporte nota de venta

--- modules/Report/Resources/views/sale_notes/report_excel.blade.php
+++ modules/Report/Resources/views/sale_notes/report_excel.blade.php
@@ -41,6 +41,17 @@
         @if(!empty($records))
             <div class="">
                 <div class=" ">
+                
+                    @php
+                        $acum_total_taxed=0;
+                        $acum_total_igv=0;
+                        $acum_total=0;
+
+                        $acum_total_taxed_usd=0;
+                        $acum_total_igv_usd=0;
+                        $acum_total_usd=0;
+                    @endphp
+
                     <table class="">
                         <thead>
                             <tr>
@@ -50,13 +61,13 @@
                                 <th>Nota de Venta</th>
                                 <th>Estado</th>
                                 <th class="text-center">Moneda</th>
+                                <th class="text-center">Comprobantes</th> 
                                 <th class="text-right" >T.Exportación</th>
                                 <th class="text-right" >T.Inafecta</th>
                                 <th class="text-right" >T.Exonerado</th>
                                 <th class="text-right">T.Gravado</th>
                                 <th class="text-right">T.Igv</th>
                                 <th class="text-right">Total</th>
-                                <th class="text-center">Comprobantes</th> 
                             </tr>
                         </thead>
                         <tbody>
@@ -68,19 +79,85 @@
                                 <td>{{$value->identifier}}</td>
                                 <td>{{$value->state_type->description}}</td>
                                 <td>{{$value->currency_type_id}}</td>
-                                <td >{{ $value->total_exportation }}</td>
-                                <td >{{ $value->total_unaffected }}</td>
-                                <td >{{ $value->total_exonerated }}</td>
-                                <td>{{ $value->total_taxed}}</td>
-                                <td>{{ $value->total_igv}}</td>
-                                <td>{{ $value->total}}</td>
                                 <td>
                                     @foreach ($value->documents as $doc)
                                          <label class="d-block">{{$doc->number_full}}</label>
                                     @endforeach
                                 </td>
+                                
+                                @if($value->state_type_id == '11')
+                                    
+                                    <td class="celda">0</td>
+                                    <td class="celda">0</td>
+                                    <td class="celda">0</td>
+                                    <td class="celda">0</td>
+                                    <td class="celda">0</td>
+                                    <td class="celda">0</td>
+
+                                @else
+
+                                    <td class="celda">{{ ($value->total_exportation) }}</td>
+                                    <td class="celda">{{ $value->total_unaffected }}</td>
+                                    <td class="celda">{{ $value->total_exonerated }}</td>
+                                    <td class="celda">{{ $value->total_taxed}}</td>
+                                    <td class="celda">{{ $value->total_igv}}</td>
+                                    <td class="celda">{{ $value->total}}</td>
+
+                                @endif 
                             </tr>
+                            
+                            @php
+                                
+                                if($value->currency_type_id == 'PEN'){
+
+                                    if($value->state_type_id == '11'){
+
+                                        $acum_total += 0;
+                                        $acum_total_taxed += 0;
+                                        $acum_total_igv += 0;
+
+                                    }else{
+
+                                        $acum_total += $value->total;
+                                        $acum_total_taxed += $value->total_taxed;
+                                        $acum_total_igv += $value->total_igv; 
+
+                                    }
+
+                                }else if($value->currency_type_id == 'USD'){
+
+                                    if($value->state_type_id == '11'){
+
+                                        $acum_total_usd += 0;
+                                        $acum_total_taxed_usd += 0;
+                                        $acum_total_igv_usd += 0;
+
+                                    }else{
+
+                                        $acum_total_usd += $value->total;
+                                        $acum_total_taxed_usd += $value->total_taxed;
+                                        $acum_total_igv_usd += $value->total_igv;
+
+                                    }
+
+                                }
+                            @endphp
+
                             @endforeach
+                            <tr>
+                                <td class="celda" colspan="9"></td>
+                                <td class="celda" >Totales PEN</td>
+                                <td class="celda">{{$acum_total_taxed}}</td>
+                                <td class="celda">{{$acum_total_igv}}</td>
+                                <td class="celda">{{$acum_total}}</td>
+                            </tr>
+                            <tr>
+                                <td class="celda" colspan="9"></td>
+                                <td class="celda" >Totales USD</td>
+                                <td class="celda">{{$acum_total_taxed_usd}}</td>
+                                <td class="celda">{{$acum_total_igv_usd}}</td>
+                                <td class="celda">{{$acum_total_usd}}</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
