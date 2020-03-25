eec7c46b74410f2ce89db216a778743ff6338c8d
cr1sti4n <cristian.ballon@gmail.com>
2020-03-25 15:15:56 -0500

    mobile

--- app/Http/Controllers/Tenant/Api/MobileController.php
+++ app/Http/Controllers/Tenant/Api/MobileController.php
@@ -192,6 +192,42 @@ class MobileController extends Controller
         ];
     }
 
+    public function searchItems(Request $request)
+    {
+
+        $items = Item::where('description', 'like', "%{$request->input}%" )->whereWarehouse()->whereNotIsSet()->whereIsActive()->orderBy('description')->get()->transform(function($row){
+
+            $full_description = ($row->internal_id)?$row->internal_id.' - '.$row->description:$row->description;
+
+            return [
+                'id' => $row->id,
+                'item_id' => $row->id,
+                'name' => $row->name,
+                'full_description' => $full_description,
+                'description' => $row->description,
+                'currency_type_id' => $row->currency_type_id,
+                'internal_id' => $row->internal_id,
+                'item_code' => $row->item_code,
+                'currency_type_symbol' => $row->currency_type->symbol,
+                'sale_unit_price' => number_format( $row->sale_unit_price, 2),
+                'purchase_unit_price' => $row->purchase_unit_price,
+                'unit_type_id' => $row->unit_type_id,
+                'sale_affectation_igv_type_id' => $row->sale_affectation_igv_type_id,
+                'purchase_affectation_igv_type_id' => $row->purchase_affectation_igv_type_id,
+                'calculate_quantity' => (bool) $row->calculate_quantity,
+                'has_igv' => (bool) $row->has_igv,
+                'is_set' => (bool) $row->is_set,
+                'aux_quantity' => 1,
+
+            ];
+        });
+
+        return [
+            'success' => true,
+            'data' => array('items' => $items)
+        ];
+    }
+
 
 
 
