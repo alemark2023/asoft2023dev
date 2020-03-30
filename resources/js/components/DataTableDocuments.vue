07c9bdce5fd39fc6ab568b8abffd80d4ef467b2d
Raul <sh.raul.jgb@gmail.com>
2020-03-30 16:28:11 -0500

    feature | doble scroll en listado de documentos

--- resources/js/components/DataTableDocuments.vue
+++ resources/js/components/DataTableDocuments.vue
@@ -3,23 +3,23 @@
         <div class="row ">
 
             <div class="col-md-12 col-lg-12 col-xl-12 ">
-                 
+
                 <div class="row">
                     <div class="col-lg-8 col-md-8 mb-2">
-                        <div class="form-group"> 
-                            <label class="control-label font-custom"><strong>Filtros de busqueda</strong></label> 
+                        <div class="form-group">
+                            <label class="control-label font-custom"><strong>Filtros de busqueda</strong></label>
                             <template v-if="!see_more">
-                                <a class="control-label font-weight-bold text-info font-custom" href="#" @click="clickSeeMore"><strong> [+ Ver más]</strong></a> 
+                                <a class="control-label font-weight-bold text-info font-custom" href="#" @click="clickSeeMore"><strong> [+ Ver más]</strong></a>
                             </template>
                             <template v-else>
-                                <a class="control-label font-weight-bold text-info font-custom" href="#" @click="clickSeeMore"><strong> [- Ver menos]</strong></a> 
+                                <a class="control-label font-weight-bold text-info font-custom" href="#" @click="clickSeeMore"><strong> [- Ver menos]</strong></a>
                             </template>
                         </div>
-                    </div> 
+                    </div>
                 </div>
-                <div class="row mt-2" v-if="see_more"> 
+                <div class="row mt-2" v-if="see_more">
                     <div class="col-lg-4 col-md-4 ">
-                        <div class="form-group"> 
+                        <div class="form-group">
                             <label class="control-label">Tipo comprobante</label>
                             <el-select v-model="search.document_type_id" @change="changeDocumentType" popper-class="el-select-document_type" filterable clearable>
                                 <el-option v-for="option in document_types" :key="option.id" :value="option.id" :label="option.description"></el-option>
@@ -31,19 +31,19 @@
                             <label class="control-label">Serie</label>
                             <el-select v-model="search.series" filterable clearable>
                                 <el-option v-for="option in series" :key="option.number" :value="option.number" :label="option.number"></el-option>
-                            </el-select> 
+                            </el-select>
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-2">
                         <div class="form-group"  >
-                            <label class="control-label">Número</label> 
+                            <label class="control-label">Número</label>
                             <el-input placeholder="Ingresar"
                                 v-model="search.number">
                             </el-input>
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-2 pb-2">
-                        <div class="form-group"> 
+                        <div class="form-group">
                             <label class="control-label">Fecha inicio </label>
 
                             <el-date-picker
@@ -56,9 +56,9 @@
                                  >
                             </el-date-picker>
                         </div>
-                    </div> 
+                    </div>
                     <div class="col-lg-2 col-md-2 pb-2">
-                        <div class="form-group"> 
+                        <div class="form-group">
                             <label class="control-label">Fecha término</label>
 
                             <el-date-picker
@@ -72,29 +72,29 @@
                                  >
                             </el-date-picker>
                         </div>
-                    </div>        
+                    </div>
 
                     <div class="col-lg-4 col-md-4 ">
-                        <div class="form-group"> 
+                        <div class="form-group">
                             <label class="control-label">Clientes</label>
-                            
+
                             <el-select v-model="search.customer_id" filterable remote  popper-class="el-select-customers"  clearable
                                 placeholder="Nombre o número de documento"
                                 :remote-method="searchRemoteCustomers"
                                 :loading="loading_search">
                                 <el-option v-for="option in customers" :key="option.id" :value="option.id" :label="option.description"></el-option>
-                            </el-select> 
+                            </el-select>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 " v-if="resource == 'documents'">
-                        <div class="form-group"> 
+                        <div class="form-group">
                             <label class="control-label">Productos</label>
                             <el-select v-model="search.item_id" filterable remote  popper-class="el-select-customers"  clearable
                                 placeholder="Nombre o código interno"
                                 :remote-method="searchRemoteItems"
                                 :loading="loading_search_item">
                                 <el-option v-for="option in items" :key="option.id" :value="option.id" :label="option.description"></el-option>
-                            </el-select> 
+                            </el-select>
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-12 pb-2" v-if="resource == 'documents'">
@@ -102,10 +102,10 @@
                             <label class="control-label">Categoría</label>
                             <el-select v-model="search.category_id" filterable clearable>
                                 <el-option v-for="option in categories" :key="option.id" :value="option.id" :label="option.name"></el-option>
-                            </el-select> 
+                            </el-select>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-2 col-sm-12 pb-2"> 
+                    <div class="col-lg-2 col-md-2 col-sm-12 pb-2">
                         <label class="control-label">Fecha de emisión</label>
                         <el-date-picker
                             v-model="search.date_of_issue"
@@ -114,37 +114,40 @@
                             placeholder="Buscar"
                             value-format="yyyy-MM-dd"
                             @change="changeDateOfIssue"    >
-                        </el-date-picker> 
+                        </el-date-picker>
                     </div>
-                    
+
                     <div class="col-lg-2 col-md-2 col-sm-12 pb-2">
                         <div class="form-group"  >
                             <label class="control-label">Estado</label>
                             <el-select v-model="search.state_type_id" filterable clearable>
                                 <el-option v-for="option in state_types" :key="option.id" :value="option.id" :label="option.description"></el-option>
-                            </el-select> 
+                            </el-select>
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-12 mt-4">
-                        <div class="form-group"  > 
+                        <div class="form-group"  >
                             <el-checkbox v-model="search.pending_payment" >PEND. DE PAGO</el-checkbox>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-md-4 col-sm-12" style="margin-top:29px"> 
+                    <div class="col-lg-4 col-md-4 col-md-4 col-sm-12" style="margin-top:29px">
                         <el-button class="submit" type="primary" @click.prevent="getRecordsByFilter" :loading="loading_submit" icon="el-icon-search" >Buscar</el-button>
                         <el-button class="submit" type="info" @click.prevent="cleanInputs"  icon="el-icon-delete" >Limpiar </el-button>
 
-                    </div>             
-                    
+                    </div>
+
                 </div>
                 <div class="row mt-1 mb-3">
-                    
-                </div> 
+
+                </div>
             </div>
 
 
             <div class="col-md-12">
-                <div class="table-responsive">
+                <div id="scroll1" style="overflow-x:auto;">
+                    <div style="height: 20px;"></div>
+                </div>
+                <div class="table-responsive" id="scroll2" style="overflow-x:auto;">
                     <table class="table">
                         <thead>
                         <slot name="heading"></slot>
@@ -177,8 +180,9 @@
 
     import moment from 'moment'
     import queryString from 'query-string'
+    import $ from 'jquery'
 
-    export default { 
+    export default {
         props: {
             resource: String,
         },
@@ -196,12 +200,12 @@
                 document_types: [],
                 categories: [],
                 state_types: [],
-                pagination: {}, 
-                search: {}, 
+                pagination: {},
+                search: {},
                 all_series: [],
                 establishment: null,
                 establishments: [],
-                series: [],                
+                series: [],
                 activePanel:0,
                 see_more:false,
                 pickerOptionsDates: {
@@ -220,7 +224,7 @@
                 this.getRecords()
             })
         },
-        async mounted () { 
+        async mounted () {
 
             await this.$http.get(`/${this.resource}/data_table`).then((response) => {
                 this.all_customers = response.data.customers
@@ -236,56 +240,58 @@
             await this.getRecords()
             await this.filterCustomers()
             await this.filterItems()
+
+            await this.cargalo()
         },
         methods: {
-            
-            searchRemoteItems(input) {  
-                
-                if (input.length > 0) { 
+
+            searchRemoteItems(input) {
+
+                if (input.length > 0) {
 
                     this.loading_search = true
                     let parameters = `input=${input}`
 
                     this.$http.get(`/documents/data-table/items?${parameters}`)
-                            .then(response => { 
+                            .then(response => {
                                 // console.log(response.data)
                                 this.items = response.data
                                 this.loading_search = false
-                                
+
                                 if(this.items.length == 0){
                                     this.filterItems()
                                 }
-                            })  
+                            })
                 } else {
                     this.filterItems()
                 }
 
             },
-            filterItems() { 
+            filterItems() {
                 this.items = this.all_items
             },
-            searchRemoteCustomers(input) {  
-                
-                if (input.length > 0) { 
+            searchRemoteCustomers(input) {
+
+                if (input.length > 0) {
 
                     this.loading_search = true
                     let parameters = `input=${input}`
 
                     this.$http.get(`/documents/data-table/customers?${parameters}`)
-                            .then(response => { 
+                            .then(response => {
                                 this.customers = response.data.customers
                                 this.loading_search = false
-                                
+
                                 if(this.customers.length == 0){
                                     this.filterCustomers()
                                 }
-                            })  
+                            })
                 } else {
                     this.filterCustomers()
                 }
 
             },
-            filterCustomers() { 
+            filterCustomers() {
                 this.customers = this.all_customers
             },
             clickSeeMore(){
@@ -293,21 +299,21 @@
             },
             initForm(){
 
-                this.search = { 
+                this.search = {
                     date_of_issue: null,
                     document_type_id:null,
                     customer_id:null,
                     item_id:null,
                     category_id:null,
                     state_type_id:null,
-                    series:null, 
-                    number:null, 
-                    d_start:null, 
-                    d_end:null, 
+                    series:null,
+                    number:null,
+                    d_start:null,
+                    d_end:null,
                     pending_payment:false,
                 }
             },
-            changeDocumentType(){                
+            changeDocumentType(){
                 this.filterSeries();
             },
             filterSeries() {
@@ -360,6 +366,15 @@
             },
             cleanInputs(){
                 this.initForm()
+            },
+            cargalo(){
+                $("#scroll1 div").width($(".table").width());
+                $("#scroll1").on("scroll", function(){
+                    $("#scroll2").scrollLeft($(this).scrollLeft());
+                });
+                $("#scroll2").on("scroll", function(){
+                    $("#scroll1").scrollLeft($(this).scrollLeft());
+                });
             }
         }
     }
