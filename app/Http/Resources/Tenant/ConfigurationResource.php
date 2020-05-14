3809c7937d6656bc9d740d327b834323898b6ebf
Raul <sh.raul.jgb@gmail.com>
2020-05-14 09:16:28 -0500

    fix | resource configuration syntax post-merge

--- app/Http/Resources/Tenant/ConfigurationResource.php
+++ app/Http/Resources/Tenant/ConfigurationResource.php
@@ -33,7 +33,7 @@ class ConfigurationResource extends JsonResource
             'enable_whatsapp' => (bool) $this->enable_whatsapp,
             'visual' => $this->visual,
             'terms_condition' => $this->terms_condition,
-            'cotizaction_finance' => (bool) $this->cotizaction_finance
+            'cotizaction_finance' => (bool) $this->cotizaction_finance,
             'include_igv' => (bool) $this->include_igv,
             'product_only_location' => (bool) $this->product_only_location,
         ];
