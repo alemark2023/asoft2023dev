0e9081e888ffe2569d68104f2b10dd9afc18cd65
b.mendoza <bemendozaa@gmail.com>
2020-06-08 18:22:02 -0500

    ajustes dashboard async

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -23,7 +23,7 @@
     {{--<link rel="dns-prefetch" href="https://fonts.gstatic.com">--}}
     {{--<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">--}}
 
-    <link defer href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link async href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
