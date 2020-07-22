f1d6e3b25c5beca1b42beab93eabbc6007ce12a6
Cristian Gonzalez <cristian.ballon@gmail.com>
2020-07-21 20:56:03 -0500

    feature | i_244 | se agrega doc asociado en reporte kardex

--- modules/Inventory/Resources/assets/js/kardex/index.vue
+++ modules/Inventory/Resources/assets/js/kardex/index.vue
@@ -13,6 +13,7 @@
                             <th>Tipo transacción</th>
                             <th>Número</th>
                             <th>NV. Asociada</th>
+                            <th>Doc. Asociado</th>
                             <th>Feha emisión</th>
                             <th>Entrada</th>
                             <th>Salida</th>
@@ -26,6 +27,7 @@
                             <td>{{row.type_transaction}}</td>
                             <td>{{row.number}}</td>
                             <td>{{row.sale_note_asoc}}</td>
+                            <td>{{row.doc_asoc}}</td>
                             <td>{{row.date_of_issue}}</td>
                             <td>{{row.input}}</td>
                             <td>{{row.output}}</td>
