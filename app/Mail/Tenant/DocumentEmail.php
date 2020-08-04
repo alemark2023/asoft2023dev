3a354398ee92714675d0c387ab5c179ddf32d106
b.mendoza <bemendozaa@gmail.com>
2020-08-04 17:10:35 -0500

    fixed | i-275 | Ajuste envio correo c. detraccion

--- app/Mail/Tenant/DocumentEmail.php
+++ app/Mail/Tenant/DocumentEmail.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\File;
 
 class DocumentEmail extends Mailable
 {
@@ -32,17 +33,17 @@ class DocumentEmail extends Mailable
         $pdf = $this->getStorage($this->document->filename, 'pdf');
         $xml = $this->getStorage($this->document->filename, 'signed');
 
-        $image_detraction = ($this->document->detraction) ? (($this->document->detraction->image_pay_constancy) ? asset('storage'.DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR.'image_detractions'.DIRECTORY_SEPARATOR.$this->document->detraction->image_pay_constancy):false):false;
+        $image_detraction = ($this->document->detraction) ? (($this->document->detraction->image_pay_constancy) ? storage_path('app'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR.'image_detractions'.DIRECTORY_SEPARATOR.$this->document->detraction->image_pay_constancy):false):false;
         
         $email = $this->subject('Envio de Comprobante de Pago Electrónico')
                     ->from(config('mail.username'), 'Comprobante electrónico')
                     ->view('tenant.templates.email.document')
                     ->attachData($pdf, $this->document->filename.'.pdf')
                     ->attachData($xml, $this->document->filename.'.xml');
-        
+
                     
         if($image_detraction){
-            return $email->attachData(file_get_contents($image_detraction), $this->document->detraction->image_pay_constancy);
+            return $email->attachData(File::get($image_detraction), $this->document->detraction->image_pay_constancy);
         }
 
         return $email;
