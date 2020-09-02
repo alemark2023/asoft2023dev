6e01bea8ea4a3c0662b2d1afcfd4ed81a6d89471
Raul <sh.raul.jgb@gmail.com>
2020-09-02 09:45:08 -0500

    fix | url tasa de cambio

--- app/CoreFacturalo/Services/Extras/ExchangeRate.php
+++ app/CoreFacturalo/Services/Extras/ExchangeRate.php
@@ -36,7 +36,8 @@ class ExchangeRate
 
         try {
 
-            $url = "http://www.sunat.gob.pe/cl-at-ittipcam/tcS01Alias?mes={$month}&anho={$year}";
+            // $url = "http://www.sunat.gob.pe/cl-at-ittipcam/tcS01Alias?mes={$month}&anho={$year}";
+            $url = "https://e-consulta.sunat.gob.pe/cl-at-ittipcam/tcS01Alias?mes={$month}&anho={$year}";
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,$url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
