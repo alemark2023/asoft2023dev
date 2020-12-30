d3304e1254d28b7955dbb2ec97979a492afa890b
b.mendoza <bemendozaa@gmail.com>
2020-12-30 18:22:05 -0500

    fix | Ajuste pdf guia default5

--- app/CoreFacturalo/Templates/pdf/default5/dispatch_a4.blade.php
+++ app/CoreFacturalo/Templates/pdf/default5/dispatch_a4.blade.php
@@ -16,7 +16,15 @@
 <body>
 <table class="full-width">
     <tr>
-        <td width="10%"></td>
+        @if($company->logo)
+            <td width="10%">
+                <img src="data:{{mime_content_type(public_path("storage/uploads/logos/{$company->logo}"))}};base64, {{base64_encode(file_get_contents(public_path("storage/uploads/logos/{$company->logo}")))}}" alt="{{$company->name}}" alt="{{ $company->name }}"  class="company_logo" style="max-width: 300px">
+            </td>
+        @else
+            <td width="10%">
+                {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo" style="max-width: 150px">--}}
+            </td>
+        @endif
         <td width="50%" class="pl-3"></td>
         <td width="40%" class="border-box p-4 text-center">
             <h4 class="text-center">{{ $document->document_type->description }}</h4>
@@ -127,54 +135,67 @@
             <td class="border-box">Empresa: {{ $document->dispatcher->name }}</td>
         </tr>
         <tr>
-            <td class="border-box">Marca:</td>
+            <td class="border-box">Placa semirremolque: {{ $document->secondary_license_plates ? $document->secondary_license_plates->semitrailer:''}}</td>
+            {{-- <td class="border-box">Marca:</td> --}}
             <td class="border-box">Conductor: {{ $document_type_driver->description }} {{ $document->driver->number }}</td>
             <td class="border-box">{{ $document_type_dispatcher->description }}: {{ $document->dispatcher->number }}</td>
         </tr>
     </tbody>
 </table>
-<table class="full-width border-box mt-10 mb-10">
+<table class="full-width mt-10 mb-10">
     <thead class="">
-    <tr>
-        <th class="border-top-bottom text-center">Código</th>
-        <th class="border-top-bottom text-left">Descripción del Articulo</th>
-        <th class="border-top-bottom text-center">Unidad de medida</th>
-        <th class="border-top-bottom text-right">Cantidad</th>
-        <th class="border-top-bottom text-center">Peso Total</th>
-    </tr>
+        <tr>
+            <th class="border-top-bottom text-center" class="cell-solid">Código</th>
+            <th class="border-top-bottom text-left" class="cell-solid">Descripción del Articulo</th>
+            <th class="border-top-bottom text-center" class="cell-solid">Unidad de medida</th>
+            <th class="border-top-bottom text-right" class="cell-solid">Cantidad</th>
+            <th class="border-top-bottom text-center" class="cell-solid">Peso Total</th>
+        </tr>
     </thead>
     <tbody>
-    @foreach($document->items as $row)
-        @php
-            $total_weight_line = 0;
-        @endphp
-        <tr>
-            <td class="text-center">{{ $row->item->internal_id }}</td>
-            <td class="text-left">{{ $row->item->description }}</td>
-            <td class="text-center">{{ $row->item->unit_type_id }}</td>
-            <td class="text-right">
-                @if(((int)$row->quantity != $row->quantity))
-                    {{ $row->quantity }}
-                @else
-                    {{ number_format($row->quantity, 0) }}
-                @endif
-            </td>
-            @if($row->relation_item->attributes)
-                @foreach($row->relation_item->attributes as $attr)
-                    @if($attr->attribute_type_id === '5032')
-                    @php
-                        $total_weight_line += $attr->value * $row->quantity;  
-                    @endphp
+        @foreach($document->items as $row)
+
+            @php
+                $total_weight_line = 0;
+            @endphp
+
+            <tr>
+                <td class="text-center" class="cell-solid" align="center" style="padding: 3px !important; margin-left: 5px !important">{{ $row->item->internal_id }}</td>
+                <td class="text-left" class="cell-solid" style="padding: 3px !important; margin-left: 5px !important">{{ $row->item->description }}</td>
+                <td class="text-left" class="cell-solid" style="padding: 3px !important; margin-left: 5px !important">{{ $row->item->unit_type_id }}</td>
+                <td class="text-right" class="cell-solid" align="right" style="padding: 3px !important; margin-left: 5px !important">
+                    @if(((int)$row->quantity != $row->quantity))
+                        {{ $row->quantity }}
+                    @else
+                        {{ number_format($row->quantity, 0) }}
                     @endif
-                @endforeach
-            @endif
-            <td class="text-center">{{$total_weight_line}}</td>
-        </tr>
-        <tr>
-            <td class="text-center"></td>
-            <td class="text-left" colspan="4">{{ $row->relation_item->name }}</td> 
-        </tr>
-    @endforeach
+                </td>
+                @if($row->relation_item->attributes)
+                    @foreach($row->relation_item->attributes as $attr)
+                        @if($attr->attribute_type_id === '5032')
+                        @php
+                            $total_weight_line += $attr->value * $row->quantity;  
+                        @endphp
+                        @endif
+                    @endforeach
+                @endif
+                <td class="text-right" class="cell-solid-rl" align="right" style="padding: 3px !important; margin-left: 5px !important">{{$total_weight_line}}</td>
+            </tr>
+
+            <tr>
+                <td class="text-center"></td>
+                @if($row->relation_item->name)
+                <td class="text-left" colspan="4" class="cell-solid" style="padding: 3px !important;">{{ $row->relation_item->name }}</td> 
+                @else
+                <td class="text-left" colspan="4" class="cell-solid" style="padding: 3px !important;"><p style="color:#ffffff !important">X</p></td> 
+                @endif
+            </tr>
+
+            <tr>
+                <td class="text-center"></td>
+                <td class="text-left" colspan="4" class="cell-solid" style="padding: 3px !important;"><p style="color:#ffffff !important">X</p></td> 
+            </tr>
+        @endforeach
     </tbody>
 </table>
 <table class="full-width mt-10 mb-10">
@@ -182,7 +203,7 @@
         <td width="60%">
             <h5 class="font-bold">OBSERVACION</h5>
             <h5>{{ $document->observations }}</h5>
-            <h5 class="text-center">_________________________________________________________________________________________</h5>
+            <h5 class="text-center">____________________________________________________________________</h5>
         </td>
         <td width="40%" class="border-box p-4 text-center" rowspan="2">
             <h4 class="text-center">___________________________________</h4>
@@ -195,29 +216,29 @@
         <td width="60%">
             <h5>El documento electrónico ha sido aceptado</h5>
             <h5></h5>
-            <h5 class="text-center">_________________________________________________________________________________________</h5>
+            <h5 class="text-center">____________________________________________________________________</h5>
         </td>
     </tr>
     <tr>
         <td width="60%">
             <h5>Representación impresa de la Guía de Remisión Electrónica Remitente</h5>
             <h5></h5>
-            <h5 class="text-center">_________________________________________________________________________________________</h5>
+            <h5 class="text-center">____________________________________________________________________</h5>
         </td>
     </tr>
 </table>
 <table class="full-width mt-10 mb-10">
     <tr>
-        <td width="30%">
+        <td width="25%">
             Usuario: {{ $document->user->name }}
         </td>
-        <td width="20%">
+        <td width="17%">
             Fecha: {{ date('d-m-Y')}}
         </td>
-        <td width="20%">
+        <td width="17%">
             Hora: {{ date('H:i:s')}}
         </td>
-        <td width="30%">
+        <td width="41%">
         </td>
     </tr>
 </table>
