26b9f11249396f3cd60ec1c29a5d51445200c89d
rash07 <sh.raul.jgb@gmail.com>
2021-03-24 11:22:35 -0500

    fixed | margen top en panel admin

--- resources/views/system/layouts/partials/sidebar.blade.php
+++ resources/views/system/layouts/partials/sidebar.blade.php
@@ -4,16 +4,7 @@
     $path[2] = (array_key_exists(2, $path)> 0)?$path[2]:'';
     $path[0] = ($path[0] === '')?'documents':$path[0];
 @endphp
-<aside id="sidebar-left" class="sidebar-left">
-    <div class="sidebar-header">
-        <div class="sidebar-title">
-            Menu
-        </div>
-        {{-- <div class="sidebar-toggle d-none d-md-block" data-toggle-class="sidebar-left-collapsed" data-target="html"
-             data-fire-event="sidebar-left-toggle">
-            <i class="fas fa-bars" aria-label="Toggle sidebar"></i>
-        </div> --}}
-    </div>
+<aside id="sidebar-left" class="sidebar-left mt-0" style="z-index: 900">
     <div class="nano">
         <div class="nano-content">
             <nav id="menu" class="nav-main" role="navigation">
