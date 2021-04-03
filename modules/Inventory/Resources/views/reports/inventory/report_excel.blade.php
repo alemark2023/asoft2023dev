216b58f1c0601fd3330b34fea072d16a4dbca0b4
Carlos Erique <eriquegasparcarlos@gmail.com>
2021-04-03 03:14:37 -0500

    fixed | reporte de inventario

--- modules/Inventory/Resources/views/reports/inventory/report_excel.blade.php
+++ modules/Inventory/Resources/views/reports/inventory/report_excel.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type"
-          content="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8"/>
+{{--    <meta http-equiv="Content-Type"--}}
+{{--          content="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8"/>--}}
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="application/pdf; charset=utf-8" />
     <title>Inventario</title>
     <style>
         body {
