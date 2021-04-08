3f4da7a18124193d74432dc54cfee72d5bc8c68b
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-04-08 14:08:55 -0500

    fixed | #433 | Filtro para buscar por numero de guia en Ventas -> Listado de Comprobantes

--- app/Http/Controllers/Tenant/DocumentController.php
+++ app/Http/Controllers/Tenant/DocumentController.php
@@ -831,6 +831,7 @@ class DocumentController extends Controller
         $item_id = $request->item_id;
         $category_id = $request->category_id;
         $purchase_order = $request->purchase_order;
+        $guides = $request->guides;
 
 
         $records = Document::query();
@@ -880,7 +881,9 @@ class DocumentController extends Controller
                                     });
                                 });
         }
-
+        if(!empty($guides)){
+            $records->where('guides','like', DB::raw("%\"number\":\"%").$guides. DB::raw("%\"%"));
+        }
         return $records;
     }
 
