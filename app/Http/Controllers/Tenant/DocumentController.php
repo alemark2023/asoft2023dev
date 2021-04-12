1df37d1525378d71a5473e7a553181fce1b7ab10
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-04-12 10:01:00 -0500

    fixed | #476 | Ajuste de validacion de configuracion ( #432 ) en el modelo y aplicado a la busqueda

--- app/Http/Controllers/Tenant/DocumentController.php
+++ app/Http/Controllers/Tenant/DocumentController.php
@@ -279,34 +279,22 @@ class DocumentController extends Controller
 
             $establishment_id = auth()->user()->establishment_id;
             $warehouse = ModuleWarehouse::where('establishment_id', $establishment_id)->first();
-
             // $items_u = Item::whereWarehouse()->whereIsActive()->whereNotIsSet()->orderBy('description')->take(20)->get();
-            $select = 'items.*';
             $items_u = Item::with('warehousePrices')
-                ->distinct()
                 ->whereIsActive()
-                ->join('item_warehouse', 'items.id', 'item_warehouse.item_id')
                 ->orderBy('description');
             $items_s = Item::with('warehousePrices')
-                ->distinct()
                 ->where('items.unit_type_id', 'ZZ')
                 ->whereIsActive()
-                ->join('item_warehouse', 'items.id', 'item_warehouse.item_id')
                 ->orderBy('description');
-
-            if (!Config::get('configuration.show_all_items_at_invoice')) {
-                $select .= ",item_warehouse.stock as stock";
-                $items_u->where('item_warehouse.warehouse_id', $establishment_id);
-                $items_s->where('item_warehouse.warehouse_id', $establishment_id);
-            }
-            if (!Config::get('configuration.show_all_items_with_out_stock')) {
-                $select .= ',item_warehouse.stock as stock';
-                $items_s->where('item_warehouse.stock', '>', 0);
-                $items_u->where('item_warehouse.stock', '>', 0);
-            }
-
-            $items_u = $items_u->select(\DB::raw($select))->take(20)->get();
-            $items_s = $items_s->select(\DB::raw($select))->take(10)->get();
+            $items_u = $items_u
+                ->WithExtraConfiguration()
+                ->take(20)
+                ->get();
+            $items_s = $items_s
+                ->WithExtraConfiguration()
+                ->take(10)
+                ->get();
             $items = $items_u->merge($items_s);
 
             return collect($items)->transform(function($row) use($warehouse){
