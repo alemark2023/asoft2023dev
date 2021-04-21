dbc6915eb22b4fcd1484f1e7859d1d7a704c279b
Carlos Erique <eriquegasparcarlos@gmail.com>
2021-04-21 09:22:11 -0500

    revision

--- modules/Pos/Http/Controllers/CashController.php
+++ modules/Pos/Http/Controllers/CashController.php
@@ -16,10 +16,8 @@ use Illuminate\Support\Facades\Mail;
 
 class CashController extends Controller
 {
-
     public function email(Request $request)
     {
-
         $request->validate(
             ['email' => 'required']
         );
@@ -34,13 +32,12 @@ class CashController extends Controller
         ];
     }
 
-
-    private function getPdf($cash, $format = 'ticket'){
-
+    private function getPdf($cash, $format = 'ticket')
+    {
         $cash = Cash::findOrFail($cash);
         $company = Company::first();
 
-        $methods_payment = collect(PaymentMethodType::all())->transform(function($row){
+        $methods_payment = collect(PaymentMethodType::all())->transform(function ($row) {
             return (object)[
                 'id' => $row->id,
                 'name' => $row->description,
@@ -50,14 +47,12 @@ class CashController extends Controller
 
         set_time_limit(0);
 
-        $quantity_rows = $cash->cash_documents()->count();
+        $quantity_rows = 30;//$cash->cash_documents()->count();
 
-        $html = view('pos::cash.report_pdf_'.$format,
-            compact("cash", "company", "methods_payment"))->render();
-
+        $html = view('pos::cash.report_pdf_' . $format,
+            compact('cash', 'company', 'methods_payment'))->render();
         $width = 78;
-
-        if($format === 'ticket') {
+        if ($format === 'ticket') {
             $pdf = new Mpdf([
                 'mode' => 'utf-8',
                 'format' => [
@@ -81,31 +76,27 @@ class CashController extends Controller
         return $pdf->output('', 'S');
     }
 
-    public function reportTicket($cash) {
-
+    public function reportTicket($cash)
+    {
         $temp = tempnam(sys_get_temp_dir(), 'cash_pdf_ticket');
         file_put_contents($temp, $this->getPdf($cash, 'ticket'));
 
         return response()->file($temp);
-
     }
 
-    public function reportA4($cash) {
-
+    public function reportA4($cash)
+    {
         $temp = tempnam(sys_get_temp_dir(), 'cash_pdf_a4');
         file_put_contents($temp, $this->getPdf($cash, 'a4'));
 
         return response()->file($temp);
-
     }
 
-
-    public function reportExcel($cash) {
-
+    public function reportExcel($cash)
+    {
         $cash = Cash::findOrFail($cash);
         $company = Company::first();
-
-        $methods_payment = collect(PaymentMethodType::all())->transform(function($row){
+        $methods_payment = collect(PaymentMethodType::all())->transform(function ($row) {
             return (object)[
                 'id' => $row->id,
                 'name' => $row->description,
@@ -117,12 +108,9 @@ class CashController extends Controller
         $filename = "Reporte_POS - {$cash->user->name} - {$cash->date_opening} {$cash->time_opening}";
 
         return (new ReportCashExport)
-                ->cash($cash)
-                ->company($company)
-                ->methods_payment($methods_payment)
-                ->download($filename.'.xlsx');
-
+            ->cash($cash)
+            ->company($company)
+            ->methods_payment($methods_payment)
+            ->download($filename . '.xlsx');
     }
-
-
 }
