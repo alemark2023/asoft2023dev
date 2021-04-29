c0343365d589cc6c1959450dcbabcfa6a8b61c0e
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-04-29 16:29:44 -0500

    fixed | #507 | Pos: Precio de item: Seleccion de precio de item por sucursales. Si no existe o es 0, se toma el precio del item

--- app/Http/Resources/Tenant/PosCollection.php
+++ app/Http/Resources/Tenant/PosCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Tenant;
 
 use App\Models\Tenant\Configuration;
+use App\Models\Tenant\ItemWarehousePrice;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PosCollection extends ResourceCollection
@@ -15,9 +16,21 @@ class PosCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return $this->collection->transform(function ($row, $key) {
-
+        $establishment_id = 0;
+        if(null !== auth()->user()) {
+            $establishment_id = auth()->user()->establishment_id;
+        }
+        return $this->collection->transform(function ($row, $key) use($establishment_id) {
             $configuration = Configuration::first();
+            $real_price = $row->sale_unit_price;
+
+            $item_warehouse_price = ItemWarehousePrice::GetItemByWarehouseAndId ($establishment_id,$row->id);
+            if (!empty($item_warehouse_price) && !empty($item_warehouse_price->price)) {
+                $real_price = $item_warehouse_price->price;
+            }
+            $price = number_format($real_price, $configuration->decimal_quantity, '.', '');
+            $edit_price = $price;
+            $aux_price = $price;
 
             return [
                 'stock' => $row->getStockByWarehouse(),
@@ -30,7 +43,7 @@ class PosCollection extends ResourceCollection
                 'currency_type_id' => $row->currency_type_id,
                 'internal_id' => $row->internal_id,
                 'currency_type_symbol' => $row->currency_type->symbol,
-                'sale_unit_price' => number_format($row->sale_unit_price, $configuration->decimal_quantity, ".", ""),
+                'sale_unit_price' => $price,
                 'purchase_unit_price' => $row->purchase_unit_price,
                 'unit_type_id' => $row->unit_type_id,
                 'sale_affectation_igv_type_id' => $row->sale_affectation_igv_type_id,
@@ -40,8 +53,8 @@ class PosCollection extends ResourceCollection
                 'is_set' => (bool) $row->is_set,
                 'edit_unit_price' => false,
                 'aux_quantity' => 1,
-                'edit_sale_unit_price' => number_format($row->sale_unit_price, $configuration->decimal_quantity, ".", ""),
-                'aux_sale_unit_price' => number_format($row->sale_unit_price, $configuration->decimal_quantity, ".", ""),
+                'edit_sale_unit_price' => $edit_price,
+                'aux_sale_unit_price' => $aux_price,
                 'image_url' => ($row->image !== 'imagen-no-disponible.jpg') ? asset('storage' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'items' . DIRECTORY_SEPARATOR . $row->image) : asset("/logo/{$row->image}"),
                 'warehouses' => collect($row->warehouses)->transform(function ($row) {
                     return [
--- app/Models/Tenant/ItemWarehousePrice.php
+++ app/Models/Tenant/ItemWarehousePrice.php
@@ -7,6 +7,84 @@ use Hyn\Tenancy\Abstracts\TenantModel;
 class ItemWarehousePrice extends TenantModel
 {
     protected $table = 'item_warehouse_prices';
-
+    protected $fillable = [
+        'item_id',
+        'warehouse_id',
+        'price',
+    ];
     public $timestamps = false;
+
+    /**
+     * @return int
+     */
+    public function getItemId()
+    {
+        return $this->item_id;
+    }
+
+    /**
+     * @param int $item_id
+     *
+     * @return ItemWarehousePrice
+     */
+    public function setItemId($item_id)
+    {
+        $this->item_id = $item_id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWarehouseId()
+    {
+        return $this->warehouse_id;
+    }
+
+    /**
+     * @param int $warehouse_id
+     *
+     * @return ItemWarehousePrice
+     */
+    public function setWarehouseId($warehouse_id)
+    {
+        $this->warehouse_id = $warehouse_id;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     *
+     * @return ItemWarehousePrice
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    /**
+     * Se obtiene el item basado en el almacen y el id del producto.
+     *
+     * @param int $warehouse
+     * @param int $item_id
+     *
+     * @return ItemWarehousePrice|null
+     */
+    public static function GetItemByWarehouseAndId($warehouse = 0, $item_id = 0)
+    {
+        $where = [
+            'warehouse_id' => $warehouse,
+            'item_id' => $item_id
+        ];
+        return self::where($where)->first();
+    }
 }
