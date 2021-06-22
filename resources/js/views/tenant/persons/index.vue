ab186753d991b6cc40d7b75edc861de9e3e989a2
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-06-22 12:07:32 -0500

    feature | #596 - 5 | Menor: Ajuste de estilo de codigo

--- resources/js/views/tenant/persons/index.vue
+++ resources/js/views/tenant/persons/index.vue
@@ -6,9 +6,15 @@
                 <li class="active"><span>{{ title }}</span></li>
             </ol>
             <div class="right-wrapper pull-right">
-                <button type="button" class="btn btn-custom btn-sm  mt-2 mr-2" @click.prevent="clickExport()"><i class="fa fa-download"></i> Exportar</button>
-                <button type="button" class="btn btn-custom btn-sm  mt-2 mr-2" @click.prevent="clickImport()"><i class="fa fa-upload"></i> Importar</button>
-                <button type="button" class="btn btn-custom btn-sm  mt-2 mr-2" @click.prevent="clickCreate()"><i class="fa fa-plus-circle"></i> Nuevo</button>
+                <button class="btn btn-custom btn-sm  mt-2 mr-2" type="button" @click.prevent="clickExport()"><i
+                    class="fa fa-download"></i> Exportar
+                </button>
+                <button class="btn btn-custom btn-sm  mt-2 mr-2" type="button" @click.prevent="clickImport()"><i
+                    class="fa fa-upload"></i> Importar
+                </button>
+                <button class="btn btn-custom btn-sm  mt-2 mr-2" type="button" @click.prevent="clickCreate()"><i
+                    class="fa fa-plus-circle"></i> Nuevo
+                </button>
             </div>
         </div>
         <div class="card mb-0">
@@ -34,14 +40,22 @@
                         <td class="text-right">
 
                             <template v-if="row.enabled">
-                                <button type="button" class="btn waves-effect waves-light btn-xs btn-info" @click.prevent="clickCreate(row.id)" >Editar</button>
+                                <button class="btn waves-effect waves-light btn-xs btn-info" type="button"
+                                        @click.prevent="clickCreate(row.id)">Editar
+                                </button>
                             </template>
 
                             <template v-if="typeUser === 'admin'">
-                                <button type="button" class="btn waves-effect waves-light btn-xs btn-danger" @click.prevent="clickDelete(row.id)">Eliminar</button>
+                                <button class="btn waves-effect waves-light btn-xs btn-danger" type="button"
+                                        @click.prevent="clickDelete(row.id)">Eliminar
+                                </button>
 
-                                <button type="button" class="btn waves-effect waves-light btn-xs btn-danger" @click.prevent="clickDisable(row.id)" v-if="row.enabled">Inhabilitar</button>
-                                <button type="button" class="btn waves-effect waves-light btn-xs btn-primary" @click.prevent="clickEnable(row.id)" v-else>Habilitar</button>
+                                <button v-if="row.enabled" class="btn waves-effect waves-light btn-xs btn-danger"
+                                        type="button" @click.prevent="clickDisable(row.id)">Inhabilitar
+                                </button>
+                                <button v-else class="btn waves-effect waves-light btn-xs btn-primary"
+                                        type="button" @click.prevent="clickEnable(row.id)">Habilitar
+                                </button>
 
                             </template>
                         </td>
@@ -49,10 +63,10 @@
                 </data-table>
             </div>
 
-            <persons-form :showDialog.sync="showDialog"
-                          :type="type"
+            <persons-form :api_service_token="api_service_token"
                           :recordId="recordId"
-                          :api_service_token="api_service_token"></persons-form>
+                          :showDialog.sync="showDialog"
+                          :type="type"></persons-form>
 
             <persons-import :showDialog.sync="showImportDialog"
                             :type="type"></persons-import>
@@ -66,55 +80,55 @@
 
 <script>
 
-    import PersonsForm from './form.vue'
-    import PersonsImport from './import.vue'
-    import PersonsExport from './partials/export.vue'
-    import DataTable from '../../../components/DataTable.vue'
-    import {deletable} from '../../../mixins/deletable'
+import PersonsForm from './form.vue'
+import PersonsImport from './import.vue'
+import PersonsExport from './partials/export.vue'
+import DataTable from '../../../components/DataTable.vue'
+import {deletable} from '../../../mixins/deletable'
 
-    export default {
-        mixins: [deletable],
-        props: ['type', 'typeUser','api_service_token'],
-        components: {PersonsForm, PersonsImport, PersonsExport, DataTable},
-        data() {
-            return {
-                title: null,
-                showDialog: false,
-                showImportDialog: false,
-                showExportDialog: false,
-                resource: 'persons',
-                recordId: null,
-            }
-        },
-        created() {
-            this.title = (this.type === 'customers')?'Clientes':'Proveedores'
-        },
-        methods: {
-            clickCreate(recordId = null) {
-                this.recordId = recordId
-                this.showDialog = true
-            },
-            clickImport() {
-                this.showImportDialog = true
-            },
-            clickExport() {
-                this.showExportDialog = true
-            },
-            clickDelete(id) {
-                this.destroy(`/${this.resource}/${id}`).then(() =>
-                    this.$eventHub.$emit('reloadData')
-                )
-            },
-            clickDisable(id){
-                this.disable(`/${this.resource}/enabled/${0}/${id}`).then(() =>
-                    this.$eventHub.$emit('reloadData')
-                )
-            },
-            clickEnable(id){
-                this.enable(`/${this.resource}/enabled/${1}/${id}`).then(() =>
-                    this.$eventHub.$emit('reloadData')
-                )
-            },
+export default {
+    mixins: [deletable],
+    props: ['type', 'typeUser', 'api_service_token'],
+    components: {PersonsForm, PersonsImport, PersonsExport, DataTable},
+    data() {
+        return {
+            title: null,
+            showDialog: false,
+            showImportDialog: false,
+            showExportDialog: false,
+            resource: 'persons',
+            recordId: null,
         }
+    },
+    created() {
+        this.title = (this.type === 'customers') ? 'Clientes' : 'Proveedores'
+    },
+    methods: {
+        clickCreate(recordId = null) {
+            this.recordId = recordId
+            this.showDialog = true
+        },
+        clickImport() {
+            this.showImportDialog = true
+        },
+        clickExport() {
+            this.showExportDialog = true
+        },
+        clickDelete(id) {
+            this.destroy(`/${this.resource}/${id}`).then(() =>
+                this.$eventHub.$emit('reloadData')
+            )
+        },
+        clickDisable(id) {
+            this.disable(`/${this.resource}/enabled/${0}/${id}`).then(() =>
+                this.$eventHub.$emit('reloadData')
+            )
+        },
+        clickEnable(id) {
+            this.enable(`/${this.resource}/enabled/${1}/${id}`).then(() =>
+                this.$eventHub.$emit('reloadData')
+            )
+        },
     }
+}
 </script>
