589a7674e519d2960b8186c171ab0bafea46b4c1
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-08-04 16:03:08 -0500

    fixed | - | Formato de codigo y simplificacion de querys

--- modules/Dashboard/Helpers/DashboardView.php
+++ modules/Dashboard/Helpers/DashboardView.php
@@ -265,67 +265,63 @@ class DashboardView
         $document_payments = DB::table('document_payments')
             ->select('document_id', DB::raw('SUM(payment) as total_payment'))
             ->groupBy('document_id');
-        if ($d_start && $d_end) {
-            $documents = DB::connection('tenant')
-                ->table('documents')
-                ->join('persons', 'persons.id', '=', 'documents.customer_id')
-                ->join('users', 'users.id', '=', 'documents.user_id')
-                ->leftJoinSub($document_payments, 'payments', function ($join) {
-                    $join->on('documents.id', '=', 'payments.document_id');
-                })
-                ->whereIn('state_type_id', ['01', '03', '05', '07', '13'])
-                ->whereIn('document_type_id', ['01', '03', '08'])
-                ->select(DB::raw("documents.id as id, " .
-                    "DATE_FORMAT(documents.date_of_issue, '%Y/%m/%d') as date_of_issue, " .
-                    "persons.name as customer_name, persons.id as customer_id, documents.document_type_id," .
-                    "CONCAT(documents.series,'-',documents.number) AS number_full, " .
-                    "documents.total as total, " .
-                    "IFNULL(payments.total_payment, 0) as total_payment, " .
-                    "documents.total - IFNULL(total_payment, 0)  as total_subtraction, " .
-                    "'document' AS 'type', " . "documents.currency_type_id, " . "documents.exchange_rate_sale, " . " documents.user_id, " . "users.name as username"))
-                ->where('documents.establishment_id', $establishment_id)
-                ->whereBetween('documents.date_of_issue', [$d_start, $d_end]);
-            if ($user_type == 'seller') {
-                $documents = $documents->where('user_id', $user_id_session);
-            }
-            if ($user_type == 'admin' && $user_id) {
-                $documents = $documents->whereIn('user_id', [$user_id_session, $user_id]);
-            }
-            if ($customer_id) {
-                $documents = $documents->where('customer_id', $customer_id);
-            }
-        } else {
-            $documents = DB::connection('tenant')
-                ->table('documents')
-                //->where('customer_id', $customer_id)
-                ->join('persons', 'persons.id', '=', 'documents.customer_id')
-                ->join('users', 'users.id', '=', 'documents.user_id')
-                ->leftJoinSub($document_payments, 'payments', function ($join) {
-                    $join->on('documents.id', '=', 'payments.document_id');
-                })
-                ->whereIn('state_type_id', ['01', '03', '05', '07', '13'])
-                ->whereIn('document_type_id', ['01', '03', '08'])
-                ->select(DB::raw("documents.id as id, " .
-                    "DATE_FORMAT(documents.date_of_issue, '%Y/%m/%d') as date_of_issue, " .
-                    "persons.name as customer_name, persons.id as customer_id, documents.document_type_id, " .
-                    "CONCAT(documents.series,'-',documents.number) AS number_full, " .
-                    "documents.total as total, " .
-                    "IFNULL(payments.total_payment, 0) as total_payment, " .
-                    "documents.total - IFNULL(total_payment, 0)  as total_subtraction, " .
-                    "'document' AS 'type', " . "documents.currency_type_id, " . "documents.exchange_rate_sale, " . " documents.user_id, " . " users.name as username"))
-                ->where('documents.establishment_id', $establishment_id);
-            if ($user_type == 'seller') {
-                $documents = $documents->where('user_id', $user_id_session);
-            }
-            if ($user_type == 'admin' && $user_id) {
-                $documents = $documents->whereIn('user_id', [$user_id_session, $user_id]);
-            }
-            if ($customer_id) {
-                $documents = $documents->where('customer_id', $customer_id);
-            }
+        $document_select = "documents.id as id, " .
+            "DATE_FORMAT(documents.date_of_issue, '%Y/%m/%d') as date_of_issue, " .
+            "persons.name as customer_name,".
+            "persons.id as customer_id,".
+            "documents.document_type_id," .
+            "CONCAT(documents.series,'-',documents.number) AS number_full, " .
+            "documents.total as total, " .
+            "IFNULL(payments.total_payment, 0) as total_payment, " .
+            "documents.total - IFNULL(total_payment, 0)  as total_subtraction, " .
+            "'document' AS 'type', " .
+            "documents.currency_type_id, " .
+            "documents.exchange_rate_sale, " .
+            " documents.user_id, " .
+            "users.name as username";
+
+        $sale_note_select = "sale_notes.id as id, " .
+            "DATE_FORMAT(sale_notes.date_of_issue, '%Y/%m/%d') as date_of_issue, " .
+            "persons.name as customer_name,".
+            "persons.id as customer_id,".
+            "null as document_type_id," .
+            "sale_notes.filename as number_full, " .
+            "sale_notes.total as total, " .
+            "IFNULL(payments.total_payment, 0) as total_payment, " .
+            "sale_notes.total - IFNULL(total_payment, 0)  as total_subtraction, " .
+            "'sale_note' AS 'type', " .
+            "sale_notes.currency_type_id, " .
+            "sale_notes.exchange_rate_sale, " .
+            " sale_notes.user_id, " .
+            "users.name as username";
+
+        $documents = DB::connection('tenant')
+            ->table('documents')
+            //->where('customer_id', $customer_id)
+            ->join('persons', 'persons.id', '=', 'documents.customer_id')
+            ->join('users', 'users.id', '=', 'documents.user_id')
+            ->leftJoinSub($document_payments, 'payments', function ($join) {
+                $join->on('documents.id', '=', 'payments.document_id');
+            })
+            ->whereIn('state_type_id', ['01', '03', '05', '07', '13'])
+            ->whereIn('document_type_id', ['01', '03', '08'])
+            ->select(DB::raw($document_select))
+            ->where('documents.establishment_id', $establishment_id);
+        if ($user_type == 'seller') {
+            $documents->where('user_id', $user_id_session);
         }
+        if ($user_type == 'admin' && $user_id) {
+            $documents->whereIn('user_id', [$user_id_session, $user_id]);
+        }
+        if ($customer_id) {
+            $documents->where('customer_id', $customer_id);
+        }
+        if ($d_start && $d_end) {
+            $documents->whereBetween('documents.date_of_issue', [$d_start, $d_end]);
+        }
+
         if ($payment_method_type_id) {
-            $documents = $documents->where('payment_method_type_id', $payment_method_type_id);
+            $documents->where('payment_method_type_id', $payment_method_type_id);
         }
         /*
          * Sale Notes
@@ -333,67 +329,32 @@ class DashboardView
         $sale_note_payments = DB::table('sale_note_payments')
             ->select('sale_note_id', DB::raw('SUM(payment) as total_payment'))
             ->groupBy('sale_note_id');
+
+
+        $sale_notes = DB::connection('tenant')
+            ->table('sale_notes')
+            // ->where('customer_id', $customer_id)
+            ->join('persons', 'persons.id', '=', 'sale_notes.customer_id')
+            ->join('users', 'users.id', '=', 'sale_notes.user_id')
+            ->leftJoinSub($sale_note_payments, 'payments', function ($join) {
+                $join->on('sale_notes.id', '=', 'payments.sale_note_id');
+            })
+            ->whereIn('state_type_id', ['01', '03', '05', '07', '13'])
+            ->select(DB::raw($sale_note_select))
+            ->where('sale_notes.establishment_id', $establishment_id)
+            ->where('sale_notes.changed', false)
+            ->where('sale_notes.total_canceled', false);
+        if ($user_type == 'seller') {
+            $sale_notes->where('user_id', $user_id_session);
+        }
+        if ($user_type == 'admin' && $user_id) {
+            $sale_notes->whereIn('user_id', [$user_id_session, $user_id]);
+        }
+        if ($customer_id) {
+            $sale_notes->where('customer_id', $customer_id);
+        }
         if ($d_start && $d_end) {
-            $sale_notes = DB::connection('tenant')
-                ->table('sale_notes')
-                //->where('customer_id', $customer_id)
-                ->join('persons', 'persons.id', '=', 'sale_notes.customer_id')
-                ->join('users', 'users.id', '=', 'sale_notes.user_id')
-                ->leftJoinSub($sale_note_payments, 'payments', function ($join) {
-                    $join->on('sale_notes.id', '=', 'payments.sale_note_id');
-                })
-                ->whereIn('state_type_id', ['01', '03', '05', '07', '13'])
-                ->select(DB::raw("sale_notes.id as id, " .
-                    "DATE_FORMAT(sale_notes.date_of_issue, '%Y/%m/%d') as date_of_issue, " .
-                    "persons.name as customer_name, persons.id as customer_id, null as document_type_id," .
-                    "sale_notes.filename as number_full, " .
-                    "sale_notes.total as total, " .
-                    "IFNULL(payments.total_payment, 0) as total_payment, " .
-                    "sale_notes.total - IFNULL(total_payment, 0)  as total_subtraction, " .
-                    "'sale_note' AS 'type', " . "sale_notes.currency_type_id, " . "sale_notes.exchange_rate_sale, " . " sale_notes.user_id, " . "users.name as username"))
-                ->where('sale_notes.establishment_id', $establishment_id)
-                ->where('sale_notes.changed', false)
-                ->whereBetween('sale_notes.date_of_issue', [$d_start, $d_end])
-                ->where('sale_notes.total_canceled', false);
-            if ($user_type == 'seller') {
-                $sale_notes = $sale_notes->where('user_id', $user_id_session);
-            }
-            if ($user_type == 'admin' && $user_id) {
-                $sale_notes = $sale_notes->whereIn('user_id', [$user_id_session, $user_id]);
-            }
-            if ($customer_id) {
-                $sale_notes = $sale_notes->where('customer_id', $customer_id);
-            }
-        } else {
-            $sale_notes = DB::connection('tenant')
-                ->table('sale_notes')
-                // ->where('customer_id', $customer_id)
-                ->join('persons', 'persons.id', '=', 'sale_notes.customer_id')
-                ->join('users', 'users.id', '=', 'sale_notes.user_id')
-                ->leftJoinSub($sale_note_payments, 'payments', function ($join) {
-                    $join->on('sale_notes.id', '=', 'payments.sale_note_id');
-                })
-                ->whereIn('state_type_id', ['01', '03', '05', '07', '13'])
-                ->select(DB::raw("sale_notes.id as id, " .
-                    "DATE_FORMAT(sale_notes.date_of_issue, '%Y/%m/%d') as date_of_issue, " .
-                    "persons.name as customer_name, persons.id as customer_id, null as document_type_id," .
-                    "sale_notes.filename as number_full, " .
-                    "sale_notes.total as total, " .
-                    "IFNULL(payments.total_payment, 0) as total_payment, " .
-                    "sale_notes.total - IFNULL(total_payment, 0)  as total_subtraction, " .
-                    "'sale_note' AS 'type', " . "sale_notes.currency_type_id, " . "sale_notes.exchange_rate_sale, " . " sale_notes.user_id, " . "users.name as username"))
-                ->where('sale_notes.establishment_id', $establishment_id)
-                ->where('sale_notes.changed', false)
-                ->where('sale_notes.total_canceled', false);
-            if ($user_type == 'seller') {
-                $sale_notes = $sale_notes->where('user_id', $user_id_session);
-            }
-            if ($user_type == 'admin' && $user_id) {
-                $sale_notes = $sale_notes->whereIn('user_id', [$user_id_session, $user_id]);
-            }
-            if ($customer_id) {
-                $sale_notes = $sale_notes->where('customer_id', $customer_id);
-            }
+            $sale_notes->whereBetween('sale_notes.date_of_issue', [$d_start, $d_end]);
         }
         // return $documents->union($sale_notes);
         return $documents->union($sale_notes)->havingRaw('total_subtraction > 0');
