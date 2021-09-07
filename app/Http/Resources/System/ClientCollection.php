3db804a9fb3ba5ab4767a223403f3fc2eba8b952
b.mendoza <bemendozaa@gmail.com>
2021-09-07 17:26:32 -0500

    feature | #898 | Admin: se agrega notificacion cpe por anular

--- app/Http/Resources/System/ClientCollection.php
+++ app/Http/Resources/System/ClientCollection.php
@@ -39,6 +39,7 @@ class ClientCollection extends ResourceCollection
                 'soap_type' => $row->soap_type,
                 'document_regularize_shipping' => $row->document_regularize_shipping,
                 'document_not_sent' => $row->document_not_sent,
+                'document_to_be_canceled' => $row->document_to_be_canceled,
             ];
         });
     }
