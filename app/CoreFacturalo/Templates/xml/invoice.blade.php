b99a9e906e1d31e5e26cd4f42e87febb4426e3cd
b.mendoza <bemendozaa@gmail.com>
2021-09-14 18:07:12 -0500

    fixed | #931 | Ajustes anticipos gravados

--- app/CoreFacturalo/Templates/xml/invoice.blade.php
+++ app/CoreFacturalo/Templates/xml/invoice.blade.php
@@ -210,7 +210,8 @@
     @foreach($document->prepayments as $prepayment)
     <cac:PrepaidPayment>
         <cbc:ID>{{ $loop->iteration }}</cbc:ID>
-        <cbc:PaidAmount currencyID="{{ $document->currency_type_id }}">{{ $prepayment->amount }}</cbc:PaidAmount>
+        <cbc:PaidAmount currencyID="{{ $document->currency_type_id }}">{{ $prepayment->total }}</cbc:PaidAmount>
+        {{-- <cbc:PaidAmount currencyID="{{ $document->currency_type_id }}">{{ $prepayment->amount }}</cbc:PaidAmount> --}}
     </cac:PrepaidPayment>
     @endforeach
     @endif
