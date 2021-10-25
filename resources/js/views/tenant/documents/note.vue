80cb4d05af2bdadab215f3439cb4e02bc22b4883
b.mendoza <bemendozaa@gmail.com>
2021-10-25 18:21:51 -0500

    feature | #930 | Agregando nota credito tipo 13

--- resources/js/views/tenant/documents/note.vue
+++ resources/js/views/tenant/documents/note.vue
@@ -43,7 +43,7 @@
                             <template v-else>
                                 <div class="form-group" :class="{'has-danger': errors['note.note_credit_type_id']}">
                                     <label class="control-label">Tipo nota de crédito</label>
-                                    <el-select v-model="form.note_credit_or_debit_type_id">
+                                    <el-select v-model="form.note_credit_or_debit_type_id" @change="changeNoteCreditType">
                                         <el-option v-for="option in note_credit_types" :key="option.id" :value="option.id" :label="option.description"></el-option>
                                     </el-select>
                                     <small class="form-control-feedback" v-if="errors['note.note_credit_type_id']" v-text="errors['note.note_credit_type_id'][0]"></small>
@@ -163,13 +163,87 @@
                             <p class="text-right" v-if="form.total_taxed > 0">OP.GRAVADA: {{ currency_type.symbol }} {{ form.total_taxed }}</p>
                             <p class="text-right" v-if="form.total_igv > 0">IGV: {{ currency_type.symbol }} {{ form.total_igv }}</p>
                             <p class="text-right" v-if="form.total_charge > 0">OTROS CARGOS: {{ currency_type.symbol }} {{ form.total_charge }}</p>
-                            <h3 class="text-right" v-if="form.total > 0"><b>TOTAL A PAGAR: </b>{{ currency_type.symbol }} {{ form.total }}</h3>
+
+                            <template v-if="isCreditNoteAndType13">
+                                <h3 class="text-right"><b>TOTAL A PAGAR: </b>{{ currency_type.symbol }} {{ form.total }}</h3>
+                            </template>
+                            <template v-else>
+                                <h3 class="text-right" v-if="form.total > 0"><b>TOTAL A PAGAR: </b>{{ currency_type.symbol }} {{ form.total }}</h3>
+                            </template>
+                        </div>
+                        
+                        <div class="col-md-12">
+
+                            <tr v-if="isCreditNoteAndType13">
+
+                                <!-- Metodos de pago -->
+                                <td class="p-0"
+                                    colspan="2">
+                                    <!-- Crédito con cuotas -->
+                                    <div v-if="form.payment_condition_id === '02'">
+                                        <table v-if="form.fee.length>0"
+                                                class="text-left"
+                                                width="100%">
+                                            <thead>
+                                            <tr>
+                                                <th class="text-left"
+                                                    style="width: 100px">Fecha
+                                                </th>
+                                                <th class="text-left"
+                                                    style="width: 100px">Monto
+                                                </th>
+                                                <th style="width: 30px"></th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr v-for="(row, index) in form.fee"
+                                                :key="index">
+                                                <td>
+                                                    <el-date-picker v-model="row.date"
+                                                                    :clearable="false"
+                                                                    format="dd/MM/yyyy"
+                                                                    type="date"
+                                                                    value-format="yyyy-MM-dd"></el-date-picker>
+                                                </td>
+                                                <td>
+                                                    <el-input v-model="row.amount"></el-input>
+                                                </td>
+                                                <td class="text-center">
+                                                    <button v-if="index > 0"
+                                                            class="btn waves-effect waves-light btn-xs btn-danger"
+                                                            type="button"
+                                                            @click.prevent="clickRemoveFee(index)">
+                                                        <i class="fa fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="5">
+                                                    <label class="control-label">
+                                                        <a class=""
+                                                            href="#"
+                                                            @click.prevent="clickAddFee"><i class="fa fa-plus font-weight-bold text-info"></i>
+                                                            <span style="color: #777777">Agregar cuota</span></a>
+
+                                                    </label>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
                         </div>
                     </div>
                 </div>
                 <div class="form-actions text-right mt-4">
                     <el-button @click.prevent="close()">Cancelar</el-button>
-                    <el-button type="primary" native-type="submit" :loading="loading_submit" v-if="form.items.length > 0 && form.total > 0">Generar</el-button>
+                    <template v-if="isCreditNoteAndType13">
+                        <el-button type="primary" native-type="submit" :loading="loading_submit" v-if="form.items.length > 0">Generar</el-button>
+                    </template>
+                    <template v-else>
+                        <el-button type="primary" native-type="submit" :loading="loading_submit" v-if="form.items.length > 0 && form.total > 0">Generar</el-button>
+                    </template>
                 </div>
             </form>
         </div>
@@ -198,6 +272,7 @@
     import DocumentFormItem from './partials/item.vue'
     import DocumentOptions from '../documents/partials/options.vue'
     import {functions, exchangeRate} from '../../../mixins/functions'
+    import {calculateRowItem} from '../../../helpers/functions'
 
     export default {
         components: {DocumentFormItem, DocumentOptions},
@@ -227,6 +302,8 @@
                 operation_types: [],
                 is_contingency: false,
                 affected_documents: [],
+                temp_total:0,
+                selected_credit_note_type_13: false,
             }
         },
         created() {
@@ -253,11 +330,181 @@
 
              this.getCustomer()
              this.getHasDocuments()
+
+
         },
         mounted() {
 
         },
+        computed: { 
+            isCreditNoteAndType13: function () {
+                return (this.form.document_type_id === '07' && this.form.note_credit_or_debit_type_id === '13')
+            },
+        }, 
         methods: {
+            onPrepareIndividualItem(data) {
+
+                let new_item = data.item
+                new_item.currency_type_id = this.form.currency_type_id
+                new_item.sale_affectation_igv_type_id = data.affectation_igv_type_id
+                new_item.sale_unit_price = 0
+                new_item.unit_price = 0
+
+                return new_item
+            },
+            onPrepareItems(items) {
+                return items.map(i => {
+
+                    i.unit_price_value = 0;
+                    i.input_unit_price_value = 0;
+                    i.discounts = (i.discounts) ? Object.values(i.discounts) : []
+                    i.charges = i.charges || [];
+                    i.attributes = i.attributes || [];
+                    i.item.id = i.item_id;
+                    i.additional_information = this.onPrepareAdditionalInformation(i.additional_information);
+                    i.item = this.onPrepareIndividualItem(i);
+                    return i;
+                });
+            },
+            onPrepareAdditionalInformation(data) {
+
+                let obs = null
+
+                if (Array.isArray(data)) {
+                    if (data.length > 0) {
+                        if (data[0] == '') {
+                            return obs;
+                        }
+                    }
+                    obs = data.join('|')
+                }
+
+                return obs;
+            },
+            async recalculateItems() {
+                
+                let items = []
+                
+                await this.onPrepareItems(this.form.items).forEach((row) => {
+                    items.push(calculateRowItem(row, this.form.currency_type_id, this.form.exchange_rate_sale))
+                })
+
+                this.form.items = items
+                this.calculateTotal()
+            },
+            clickRemoveFee(index) {
+                this.form.fee.splice(index, 1);
+                this.calculateFee();
+            },
+            clickAddFee() {
+                this.form.date_of_due = moment().format('YYYY-MM-DD');
+                this.form.fee.push({
+                    id: null,
+                    date: moment().format('YYYY-MM-DD'),
+                    currency_type_id: this.form.currency_type_id,
+                    amount: 0,
+                });
+                this.calculateFee();
+            },
+            calculateFee() {
+                let fee_count = this.form.fee.length;
+                let total = this.temp_total;
+                // let total = this.form.total;
+
+                let accumulated = 0;
+                let amount = _.round(total / fee_count, 2);
+                _.forEach(this.form.fee, row => {
+                    accumulated += amount;
+                    if (total - accumulated < 0) {
+                        amount = _.round(total - accumulated + amount, 2);
+                    }
+                    row.amount = amount;
+                })
+            },
+            changeNoteCreditType(){
+                
+                if(this.isCreditNoteAndType13){
+
+                    this.form.payment_condition_id = '02'
+
+                    //cuando es tipo de nota credito = 13, se deben enviar los valores en 0, por ello se recalculan todos los montos
+                    this.clickAddFee()
+                    this.recalculateItems()
+
+                    // variable usada para determinar si al menos usaron una vez el tipo de nota 13
+                    this.selected_credit_note_type_13 = true
+
+                }else{
+
+                    // si se seleccionó el tipo de nota 13, se deberá reiniciar la data
+                    if(this.selected_credit_note_type_13) this.initData()
+                
+                }
+
+            },
+            async initData() {
+
+                this.form.payment_condition_id = null
+                this.form.fee = []
+                await this.getNote()
+                await this.initFormCreditNoteAndType13()
+                this.form.operation_type_id = (this.operation_types.length > 0)?this.operation_types[0].id:null
+                this.form.document_type_id = (this.document_types.length > 0)?this.document_types[0].id:null
+                this.form.series_id = (this.series.length > 0)?this.series[0].id:null
+
+            },
+            async initFormCreditNoteAndType13() {
+                
+                this.errors = {}
+
+                this.form.establishment_id= this.document.establishment_id
+                this.form.document_type_id= null
+                this.form.series_id= null
+                this.form.number = '#'
+                this.form.date_of_issue= moment().format('YYYY-MM-DD')
+                this.form.time_of_issue= moment().format('HH:mm:ss')
+                this.form.customer_id= this.document.customer_id
+                this.form.currency_type_id= this.document.currency_type_id
+                this.form.purchase_order= null
+                this.form.exchange_rate_sale= this.document.exchange_rate_sale
+                this.form.total_prepayment =this.document.total_prepayment
+                this.form.total_charge= this.document.total_charge
+                this.form.total_discount= this.document.total_discount
+                this.form.total_exportation= this.document.total_exportation
+                this.form.total_free= this.document.total_free
+                this.form.total_taxed= this.document.total_taxed
+                this.form.total_unaffected= this.document.total_unaffected
+                this.form.total_exonerated= this.document.total_exonerated
+                this.form.total_igv= this.document.total_igv
+                this.form.total_base_isc= this.document.total_base_isc
+                this.form.total_isc= this.document.total_isc
+                this.form.total_base_other_taxes= this.document.total_base_other_taxes
+                this.form.total_other_taxes= this.document.total_other_taxes
+                this.form.total_plastic_bag_taxes= this.document.total_plastic_bag_taxes
+                this.form.total_taxes= this.document.total_taxes
+                this.form.total_value= this.document.total_value
+                this.form.total= this.document.total
+                this.form.items= this.document.items
+                this.form.affected_document_id= this.document.id
+                this.form.note_description= null
+                this.form.actions= {
+                    format_pdf: 'a4'
+                }
+                this.form.operation_type_id= null
+                this.form.hotel= {}
+                this.form.charges= this.document.charges ? Object.values(this.document.charges) : null
+                this.form.payment_condition_id = null
+                this.form.fee = []
+
+                await this.form.items.forEach((item)=>{
+                    item.input_unit_price_value = item.unit_price
+                    item.additional_information = null
+                    item.IdLoteSelected = item.item.IdLoteSelected
+                })
+
+                this.temp_total = this.form.total
+
+            },
             async initForm() {
                 this.errors = {}
                 this.form = {
@@ -298,6 +545,8 @@
                     operation_type_id: null,
                     hotel: {},
                     charges: this.document.charges ? Object.values(this.document.charges) : null,
+                    payment_condition_id : null,
+                    fee : [],
                 }
 
 
@@ -306,6 +555,9 @@
                     item.additional_information = null
                     item.IdLoteSelected = item.item.IdLoteSelected
                 })
+
+                this.temp_total = this.form.total
+                this.selected_credit_note_type_13 = false
 
             },
             clickAddItemNote(){
@@ -328,6 +580,7 @@
             },
             async resetForm() {
                 await this.getNote()
+                await this.getHasDocuments()
                 await this.initForm()
                 this.form.operation_type_id = (this.operation_types.length > 0)?this.operation_types[0].id:null
                 this.form.document_type_id = (this.document_types.length > 0)?this.document_types[0].id:null
@@ -339,7 +592,7 @@
                     .then(response => {
                         // console.log(response)
                         this.document = response.data
-                        this.getHasDocuments()
+                        // this.getHasDocuments()
                     })
             },
             getHasDocuments(){
@@ -383,8 +636,9 @@
                     });
                 }
 
-
                 this.form.series_id = (this.series.length > 0)?this.series[0].id:null
+
+                // this.changeNoteCreditType()
             },
             changeDateOfIssue() {
                 this.searchExchangeRateByDate(this.form.date_of_issue).then(response => {
