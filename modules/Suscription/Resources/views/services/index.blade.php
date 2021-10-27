90d4ab6c1ef15bea445b81d544fc34572137a47b
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-10-27 10:40:12 -0500

    Feature | #952 | Servicios: Será tratado como servicios directamente.

--- modules/Suscription/Resources/views/services/index.blade.php
+++ modules/Suscription/Resources/views/services/index.blade.php
@@ -2,10 +2,17 @@
 
 @section('content')
 
+    <tenant-items-index
+        type="{{ 'ZZ' }}"
+        :configuration="{{\App\Models\Tenant\Configuration::first()->toJson()}}"
+        :type-user="{{json_encode(Auth::user()->type)}}"
+    ></tenant-items-index>
 
+    <!--
     <tenant-suscription-service-index
         :configuration="{{\App\Models\Tenant\Configuration::getPublicConfig()}}"
     ></tenant-suscription-service-index>
 
+    -->
 
 @endsection
