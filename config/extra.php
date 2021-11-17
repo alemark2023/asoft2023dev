540625788b4b341b77d0f7d7c2d11814ac5b833d
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-11-17 13:46:28 -0500

    Feature | - | Config: number_items_at_start por defecto 20

--- config/extra.php
+++ config/extra.php
@@ -2,7 +2,7 @@
 
 return [
     'number_items_in_search' =>env('NUMBER_SEARCH_ITEMS', 250),
-    'number_items_at_start' =>env('NUMBER_ITEMS', 50),
+    'number_items_at_start' =>env('NUMBER_ITEMS', 20),
     'extra_log' =>env('EXTRA_LOG', false),
     'apk_url' =>env('APK_URL', 'https://facturaloperu.com/apk/app-debug.apk'),
     'wiki_pharmacy' =>env('WIKI_PHARMACY', 'https://gitlab.com/carlomagno83/facturadorpro4/-/wikis/Modulo-Farmacia'),
