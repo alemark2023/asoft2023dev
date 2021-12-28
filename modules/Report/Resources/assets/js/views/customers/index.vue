ed2e83637eabc9db0b8c1fc1a8ce18019e44c799
Carlos Alvarado <alvaradocarlo@gmail.com>
2021-12-28 13:22:23 -0500

    Feature | #1314 | Reporte de Documento por Cliente: Añadiendo placa

--- modules/Report/Resources/assets/js/views/customers/index.vue
+++ modules/Report/Resources/assets/js/views/customers/index.vue
@@ -11,47 +11,49 @@
                             <th class="">Fecha</th>
                             <th class="">Tipo Documento</th>
                             <th class="">Moneda</th>
+                            <th class="">Número de Placa</th>
                             <th class="">Serie</th>
                             <th class="">Número</th>
                             <th class="">Monto</th>
                         <tr>
                         <tr slot-scope="{ index, row }">
-                            <td>{{ index }}</td> 
+                            <td>{{ index }}</td>
                             <td>{{row.date_of_issue}}</td>
                             <td>{{row.document_type_description}}</td>
                             <td>{{row.currency_type_id}}</td>
+                            <td>{{row.plate_number}}</td>
                             <td>{{row.series}}</td>
                             <td>{{row.alone_number}}</td>
-                            <td>{{ (row.document_type_id == '07') ? ( (row.total == 0) ? '0.00': '-'+row.total) : ((row.document_type_id!='07' && (row.state_type_id =='11'||row.state_type_id =='09')) ? '0.00':row.total) }}</td> 
+                            <td>{{ (row.document_type_id == '07') ? ( (row.total == 0) ? '0.00': '-'+row.total) : ((row.document_type_id!='07' && (row.state_type_id =='11'||row.state_type_id =='09')) ? '0.00':row.total) }}</td>
                         </tr>
-                        
+
                     </data-table>
-                     
-                    
-                </div> 
+
+
+                </div>
         </div>
- 
+
     </div>
 </template>
 
 <script>
- 
+
     import DataTable from '../../components/DataTableCustomers.vue'
 
-    export default { 
+    export default {
         components: {DataTable},
         data() {
             return {
-                resource: 'reports/customers',                 
-                form: {}, 
+                resource: 'reports/customers',
+                form: {},
 
             }
         },
-        async created() { 
+        async created() {
         },
-        methods: { 
-             
-            
+        methods: {
+
+
         }
     }
 </script>
