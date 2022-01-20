30993ab1c7843b8b1b38d5d848cb9b0c239b094a
Carlos Alvarado <alvaradocarlo@gmail.com>
2022-01-20 12:17:42 -0500

    Fixed | - | Templates: Separando fecha de lote con lote

--- app/Services/ItemLotsGroupService.php
+++ app/Services/ItemLotsGroupService.php
@@ -1,33 +1,58 @@
 <?php
 
-namespace App\Services;
-use Modules\Item\Models\ItemLotsGroup;
+    namespace App\Services;
 
-class ItemLotsGroupService
-{
+    use Modules\Item\Models\ItemLotsGroup;
 
-    public function getLote($id)
+    class ItemLotsGroupService
     {
-        $result = '';
 
-        if(is_array($id)) {
-
-            foreach ($id as $item) {
-                $result .= "/" . $item->code . " V:" . $item->date_of_due;
+        /**
+         * Devuelve un string con solo lotes
+         *
+         * @param $id
+         *
+         * @return \Illuminate\Database\Eloquent\HigherOrderBuilderProxy|mixed|string
+         */
+        public function getLote($id)
+        {
+            $result = '';
+            if (is_array($id)) {
+                foreach ($id as $item) {
+                    $result .= "/" . $item->code;
+                }
+            } else {
+                $record = ItemLotsGroup::where('id', $id)->first();
+                if ($record) {
+                    $result = $record->code;
+                }
             }
+            return $result;
         }
-        else {
-            $record = ItemLotsGroup::where('id', $id)->first();
 
-            if($record)
-            {
-                $result = $record->code . " V:" . $record->date_of_due;
+        /**
+         * Devuelve un string con Lote y fecha de vencimiento.
+         *
+         * @param $id
+         *
+         * @return string
+         */
+        public function getLoteWithDate($id)
+        {
+            $result = '';
+            if (is_array($id)) {
+                foreach ($id as $item) {
+                    $result .= "/" . $item->code . " V:" . $item->date_of_due;
+                }
+            } else {
+                $record = ItemLotsGroup::where('id', $id)->first();
+
+                if ($record) {
+                    $result = $record->code . " V:" . $record->date_of_due;
+                }
             }
+            return $result;
         }
-        
 
-        return $result;
+
     }
-
-
-}
