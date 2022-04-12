584f362f35f45abc9cc3aa4de85c9ed74a377624
Jocelyme Lucas <jocelymelucas@gmail.com>
2022-04-12 11:46:57 -0500

    feature | #1554 | modulo hotel - actualizacion de estado pendiente a pagado

--- modules/Hotel/Http/Controllers/HotelRentController.php
+++ modules/Hotel/Http/Controllers/HotelRentController.php
@@ -160,11 +160,19 @@ class HotelRentController extends Controller
 	public function finalizeRent($rentId)
 	{
 		$rent = HotelRent::findOrFail($rentId);
+		$items = HotelRentItem::where('hotel_rent_id', $rentId)
+		->where('type', 'PRO')->get();
 		$rent->update([
 			'arrears' => request('arrears'),
 			'payment_status' => 'PAID',
 			'status'  => 'FINALIZADO'
 		]);
+		foreach ($items as $item) {
+			$item->payment_status = 'PAID';
+		}
+		/* $item->update([
+			'payment_status' => 'PAID'
+		]); */
 		HotelRoom::where('id', $rent->hotel_room_id)
 			->update([
 				'status' => 'LIMPIEZA'
