3369ae702486f62eac31d8d4d9c94671a033e4aa
Jocelyme Lucas <jocelymelucas@gmail.com>
2022-05-18 10:46:47 -0500

    feature | #1517 | historial de ventas productos

--- modules/Pos/Http/Controllers/HistoryController.php
+++ modules/Pos/Http/Controllers/HistoryController.php
@@ -20,26 +20,30 @@ class HistoryController extends Controller
 
         $form = json_decode($request->form);
         
-        $sale_notes = SaleNoteItem::where('item_id', $form->item_id)
-                                    ->whereHas('sale_note', function($query) use($form){
-                                        $query->where('customer_id', $form->customer_id);
-                                    })
-                                    ->join('sale_notes', 'sale_note_items.sale_note_id', '=', 'sale_notes.id')
-                                    ->select(DB::raw("sale_note_items.id as id, sale_notes.prefix as series, sale_notes.id as number,
-                                            sale_note_items.unit_price as price, sale_notes.date_of_issue as date_of_issue"))
-                                    ->orderBy('created_at', 'desc');
+        $sale_notes = SaleNoteItem::where('item_id', $form->item_id);
+        if(!$form->all_user){
+            $sale_notes = $sale_notes->whereHas('sale_note', function($query) use($form){
+                $query->where('customer_id', $form->customer_id);
+            });
+        }
+                                    
+        $sale_notes = $sale_notes->join('sale_notes', 'sale_note_items.sale_note_id', '=', 'sale_notes.id')
+                                    ->join('persons', 'persons.id', '=', 'sale_notes.customer_id')->select(DB::raw('sale_note_items.id as id, sale_notes.prefix as series, sale_notes.id as number,
+                                    sale_note_items.unit_price as price, sale_notes.date_of_issue as date_of_issue,persons.name as name'));
 
 
-        $documents = DocumentItem::where('item_id', $form->item_id)
-                                    ->whereHas('document', function($query) use($form){
-                                        $query->where('customer_id', $form->customer_id);
-                                    })
-                                    ->join('documents', 'document_items.document_id', '=', 'documents.id')
-                                    ->select(DB::raw("document_items.id as id, documents.series as series, documents.number as number,
-                                            document_items.unit_price as price, documents.date_of_issue as date_of_issue"))
-                                    ->orderBy('created_at', 'desc');
+        $documents = DocumentItem::where('item_id', $form->item_id);
+        if(!$form->all_user){
+            $documents = $documents->whereHas('document', function($query) use($form){
+                $query->where('customer_id', $form->customer_id);
+            });
+        }
+                                    
+        $documents=$documents->join('documents', 'document_items.document_id', '=', 'documents.id')
+                                    ->join('persons', 'persons.id', '=', 'documents.customer_id')->select(DB::raw('document_items.id as id, documents.series as series, documents.number as number,
+                                    document_items.unit_price as price, documents.date_of_issue as date_of_issue,persons.name as name'));
 
-        $records = $documents->union($sale_notes)->orderBy('date_of_issue','desc')->orderBy('number','desc');
+        $records = $documents->union($sale_notes);
 
         return new HistorySalesCollection($records->paginate(config('tenant.items_per_page_simple_d_table_params')));
 
