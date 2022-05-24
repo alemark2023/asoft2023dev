26f8999bbcf7766ee90937845388d70a20edb3e5
rash07 <sh.raul.jgb@gmail.com>
2022-05-24 11:49:07 -0500

    fix textos

--- modules/Report/Resources/assets/js/views/massive-downloads/index.vue
+++ modules/Report/Resources/assets/js/views/massive-downloads/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card mb-0 pt-2 pt-md-0">
         <div class="card-header bg-info">
-            <h3 class="my-0">Descarga masiva de documentsssos</h3>
+            <h3 class="my-0">Descarga masiva de documentos</h3>
         </div>
         <div class="card mb-0">
             <div class="card-body">
