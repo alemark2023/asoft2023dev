75458703b47607a53e0d29dd72d91142e78da000
b.mendoza <bemendozaa@gmail.com>
2022-06-03 15:10:12 -0500

    feature | #757 | Se agrega configuracion para que los vendedores generen pedidos desde cotizacion

--- resources/views/tenant/quotations/index.blade.php
+++ resources/views/tenant/quotations/index.blade.php
@@ -4,7 +4,8 @@
 
     <tenant-quotations-index
     	:type-user="{{json_encode(Auth::user()->type)}}"
-    	:soap-company="{{ json_encode($soap_company) }}">
+    	:soap-company="{{ json_encode($soap_company) }}"
+    	:generate-order-note-from-quotation="{{ json_encode($generate_order_note_from_quotation) }}">
     </tenant-quotations-index>
 
 @endsection
