c3f56504076a5510ab112786f4af69f0dc0b7139
b.mendoza <bemendozaa@gmail.com>
2022-11-14 17:57:10 -0500

    feature | #1059 | se agregan permisos adicionales de usuario

--- modules/LevelAccess/Http/Controllers/Api/UserPermissionController.php
+++ modules/LevelAccess/Http/Controllers/Api/UserPermissionController.php
@@ -24,7 +24,23 @@ class UserPermissionController extends Controller
     {
         $user = User::findOrFail($id);
 
-        return $user->getWebPermissionsByUser();
+        return [
+            'other_permissions' => [
+                'permission_edit_cpe' => $user->permission_edit_cpe,
+                'recreate_documents' => $user->recreate_documents,
+                'permission_force_send_by_summary' => $user->permission_force_send_by_summary,
+            ],
+            'payments' => [
+                'create_payment' => $user->create_payment,
+                'delete_payment' => $user->delete_payment,
+            ],
+            'purchases' => [
+                'edit_purchase' => $user->edit_purchase,
+                'annular_purchase' => $user->annular_purchase,
+                'delete_purchase' => $user->delete_purchase,
+            ],
+            'modules' => $user->getWebPermissionsByUser(),
+        ];
     }
 
 }
